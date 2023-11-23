1dff107164feccf68b5b6d072585d1e43d8a8acd
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2023-11-23 13:40:06 -0600

    Update 8-4.cpp

--- ch8/8-4.cpp
+++ ch8/8-4.cpp
@@ -6,46 +6,65 @@
 #include <iomanip>
 using namespace std;
 
+const int SIZE = 5;
+void inputSales(const string[], int sales[]);
+void findLowAndHigh(int&, int&, const int[]);
+void displayReport(const string[], const int[], int, int);
+
 int main()
 {
-	string names[5] = { "mild","medium", "sweet", "hot","zesty" };
-	int sales[5];
-	int lowest, highest;
-	for (int i = 0; i < 5; i++)
+	string names[SIZE] = { "mild","medium", "sweet", "hot","zesty" };
+	int sales[SIZE];
+	int high, low;
+	inputSales(names, sales);
+	high = low = sales[0];
+	findLowAndHigh(high, low, sales);
+	displayReport(names, sales, high, low);
+	return 0;
+}
+
+void inputSales(const string names[], int sales[])
+{
+	for (int i = 0; i < SIZE; i++)
 	{
 		do
 		{
 			cout << "Enter number of jars sold in past month for "
-				<< names[i] << " salsa (greater than or equal to 0): ";
+				<< names[i] << " salsa: ";
 			cin >> sales[i];
 			if (sales[i] < 0)
-				cout << "INPUT ERROR." << endl;
-		}while (sales[i] < 0);
+				cout << "INPUT ERROR. Number of Jars sold must" 
+				<< " be greater than or equal to 0." << endl;
+		} while (sales[i] < 0);
 	}
-
-	lowest = highest = sales[0];
-	for (int i = 1; i < 5; i++)
+}
+void findLowAndHigh(int& high, int& low, const int sales[])
+{
+	for (int i = 1; i < SIZE; i++)
 	{
-		if (sales[i] > highest)
-			highest = sales[i];
-		else if (sales[i] < lowest)
-			lowest = sales[i];
+		if (sales[i] > high)
+			high = sales[i];
+		else if (sales[i] < low)
+			low = sales[i];
 	}
+}
 
+void displayReport(const string names[], const int sales[], int high, int low)
+{
 	cout << setw(20) << "Sales Report" << endl;
 	cout << "-----------------------------------" << endl;
 	cout << fixed << showpoint << setprecision(2);
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < SIZE; i++)
 		cout << names[i] << ": " << sales[i] << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
-		if (sales[i] == highest)
-			cout << "Highest Selling salsa this month is "
+		if (sales[i] == high)
+			cout << "Highest selling salsa this month is "
 			<< names[i] << " with " << sales[i] << " sold." << endl;
-		else if (sales[i] == lowest)
-			cout << "Lowest Selling salsa this month is "
+		else if (sales[i] == low)
+			cout << "Lowest selling salsa this month is "
 			<< names[i] << " with " << sales[i] << " sold." << endl;
 	}
-	return 0;
-}
 
+
+}
