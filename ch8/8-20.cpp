1995faeb0a959325cf319964e09201995b44ff0d
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2023-12-08 10:35:15 -0600

    Add files via upload

--- ch8/8-20.cpp
+++ ch8/8-20.cpp
@@ -3,14 +3,10 @@
 #include <iostream>
 #include "TicketManager.h"
 
-
 int main()
 {
     ifstream prices("C:\\Users\\p3209584\\source\\repos\\start c++ ch 8\\8-20\\8-20\\SeatPrices.dat");
     ifstream availability("C:\\Users\\p3209584\\source\\repos\\start c++ ch 8\\8-20\\8-20\\SeatAvailability.dat");
     TicketManager b(prices, availability);
-    cout << b.displaySeats();
     return 0;
 }
-
-
