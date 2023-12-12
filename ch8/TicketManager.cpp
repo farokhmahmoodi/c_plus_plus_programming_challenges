fec2a54bedba59b5348600521d70dbab8b82c3ca
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2023-12-12 16:15:37 -0600

    Update TicketManager.cpp

--- ch8/TicketManager.cpp
+++ ch8/TicketManager.cpp
@@ -42,6 +42,7 @@ void TicketManager::displaySeats()
 void TicketManager::ticketsRequest(int numSeats, int rowNum, int startingSeat)
 {
 	double total = 0.0;
+	int unavailableSeats = 0;
 	char choice;
 
 	for (int row = rowNum - 1; row < rowNum; row++)
@@ -49,30 +50,65 @@ void TicketManager::ticketsRequest(int numSeats, int rowNum, int startingSeat)
 		for (int column = startingSeat - 1,start = 0; start < numSeats; column++, start++)
 		{
 			if (a[row][column].availability == '*')
+			{
 				cout << "Seat in row " << (row + 1) << " column "
-				<< (column + 1) << " is unavailable." << endl;
+					<< (column + 1) << " is unavailable." << endl;
+				unavailableSeats++;
+				break;
+			}
 			else
 			{
 				total += a[row][column].price;
 			}
 		}
 	}
-	cout << "Number of requested seats: " << numSeats << endl;
-	cout << fixed << showpoint << setprecision(2);
-	cout << "Price per seat in requested row (" << rowNum << "): $ " << a[rowNum][startingSeat].price << endl;
-	cout << "Total price for seats: $" << total << endl;
-	cout << "Would you like to purchase these seats? (Y\\N) ";
-	cin >> choice;
-	if (toupper(choice) == 'Y')
+	if (unavailableSeats == 0)
 	{
-		validateSale(total);
+		cout << "Number of requested seats: " << numSeats << endl;
+		cout << fixed << showpoint << setprecision(2);
+		cout << "Price per seat in requested row (" << rowNum << "): $ " << a[rowNum][startingSeat].price << endl;
+		cout << "Total price for seats: $" << total << endl;
+		cout << "Would you like to purchase these seats? (Y\\N) ";
+		cin >> choice;
+		if (toupper(choice) == 'Y')
+		{
+			purchaseTickets(numSeats, rowNum, startingSeat,total);
+		}
 	}
 }
 
-void TicketManager::validateSale(double total)
+void TicketManager::purchaseTickets(int numSeats, int rowNum, int startingSeat, double total)
 {
 	double money;
-	cout << "Enter money input to purchase the seats:";
+	cout << "Enter money to purchase the seats:";
 	cin >> money;
-	
+	if (money < total)
+		cout << "Money input not sufficient to purchase the tickets." << endl;
+	else
+	{
+		cout << endl;
+		for (int row = rowNum - 1; row < rowNum; row++)
+		{
+			for (int column = startingSeat - 1, start = 0, seatNum = startingSeat; start < numSeats; column++, start++, seatNum++)
+			{
+				a[row][column].availability = '*';
+				cout << setw(15) << "Ticket Sold" << endl;
+				cout << "------------------" << endl;
+				if(rowNum < 10)
+					cout << "|" << "Row " << rowNum << setw(12) << "|" << endl;
+				else
+					cout << "|" << "Row " << rowNum << setw(11) << "|" << endl;
+				if(seatNum < 10)
+					cout << "|" << "Seat Number " << seatNum << setw(4) << "|" << endl;
+				else
+					cout << "|" << "Seat Number " << seatNum << setw(3) << "|" << endl;
+				cout << fixed << showpoint << setprecision(2);
+				if(a[row][column].price < 10.00)
+					cout << "|" << "Price $" << a[row][column].price << setw(6) << "|" << endl;
+				else
+					cout << "|" << "Price $" << a[row][column].price << setw(5) << "|" << endl;
+				cout << "------------------" << endl;
+			}
+		}
+	}	
 }
