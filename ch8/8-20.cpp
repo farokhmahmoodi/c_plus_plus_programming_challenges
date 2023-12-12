476fdc27dff006cc2b7686104096621481b84c21
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2023-12-12 17:01:34 -0600

    Update 8-20.cpp

--- ch8/8-20.cpp
+++ ch8/8-20.cpp
@@ -7,7 +7,57 @@ int main()
     ifstream prices("C:\\Users\\p3209584\\source\\repos\\start c++ ch 8\\8-20\\8-20\\SeatPrices.dat");
     ifstream availability("C:\\Users\\p3209584\\source\\repos\\start c++ ch 8\\8-20\\8-20\\SeatAvailability.dat");
     TicketManager b(prices, availability);
+    int choice;
 
-    //b.ticketsRequest(6, 7, 9);
+    do
+    {
+        do
+        {
+            cout << "Welcome to Theater Ticket Sales Program!" << endl;
+            cout << "Select option from menu below." << endl;
+            cout << "1. Display Seating Chart" << endl;
+            cout << "2. Request Tickets" << endl;
+            cout << "3. Print Sales Report" << endl;
+            cout << "4. Quit" << endl;
+            cin >> choice;
+            if (choice < 1 || choice > 4)
+                cout << "Input error. Please enter valid menu choice." << endl;
+        } while (choice < 1 || choice > 4);
+        switch (choice)
+        {
+        case 1:
+            b.displaySeats();
+            break;
+        case 2:
+            int numSeats, rowNum, seatNum;
+            do
+            {
+                cout << "Enter number of seats you want to purchase(between 1 and 450): ";
+                cin >> numSeats;
+                if (numSeats < 1 || numSeats > 450)
+                    cout << "Input error. Please enter valid number between 1 and 450." << endl;
+            } while (numSeats < 1 || numSeats > 450);
+            do
+            {
+                cout << "Enter row number of seats you want to purchase(between 1 and 15): ";
+                cin >> rowNum;
+                if(rowNum < 1 || rowNum > 15)
+                    cout << "Input error. Please enter valid number between 1 and 15." << endl;
+            } while (rowNum < 1 || rowNum > 15);
+            do
+            {
+                cout << "Enter starting seat number for seats you want to purchase(between 1 and 30): ";
+                cin >> seatNum;
+                if (seatNum < 1 || seatNum > 30)
+                    cout << "Input error. Please enter valid number between 1 and 30." << endl;
+            } while (seatNum < 1 || seatNum > 30);
+            b.ticketsRequest(numSeats, rowNum, seatNum);
+            break;
+        case 3:
+            b.salesReport();
+            break;
+        }
+    } while (toupper(choice) != 4);
+
     return 0;
 }
