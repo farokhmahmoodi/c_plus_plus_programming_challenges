6ab6318e97463f7f2e2086fd8cca174ba7597792
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2023-12-26 10:25:43 -0600

    Update 9-6.cpp

--- ch9 Searching, Sorting, and Algorithm Analysis/9-6.cpp
+++ ch9 Searching, Sorting, and Algorithm Analysis/9-6.cpp
@@ -10,8 +10,8 @@ int main()
 	const int SIZE = 20;
 
 	string name[SIZE] =
-	{ "Collins, Bill", "Smith, Bart", "Michalski, Joe", "Griffin, Jim"
-	 "Sanchez, Manny", "Rubin, Sarah", "Taylor, Tyrone", "Johnson, Jill"
+	{ "Collins, Bill", "Smith, Bart", "Michalski, Joe", "Griffin, Jim",
+	 "Sanchez, Manny", "Rubin, Sarah", "Taylor, Tyrone", "Johnson, Jill",
 	 "Allison, Jeff", "Moreno, Juan", "Wolfe, Bill", "Whitman, Jean",
 	 "Moretti, Bella", "Wu, Eric", "Patel, Renee", "Harrison, Rose",
 	 "Smith, Cathy", "Conroy, Pat", "Kelly, Sean", "Holland, Beth" };
