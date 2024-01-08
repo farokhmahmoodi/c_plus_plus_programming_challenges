8a8e3451845002a54c964dd9e9d877139b024201
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-01-08 02:19:06 -0600

    Add files via upload

---
+++ ch10 Pointers/10-5 Pie a la Mode.cpp
@@ -0,0 +1,66 @@
+/*In statistics the mode of a set of values is the value that occurs most often. 
+Write a program that determines how many pieces of pie most people eat in a year. 
+Set up an integer array that can hold responses from 30 people. For each person, enter the number of pieces they say they eat in a year. 
+Then write a function that finds the mode of these 30 values. This will be the number of pie slices eaten by the most people. 
+The function that finds and returns the mode should accept two arguments, an array of integers, and a value indicating how many elements 
+are in the array.*/
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 10;
+
+void getData(int[], int);
+int findMode(int[], int);
+
+int main()
+{
+    int a[SIZE], mode;
+
+    getData(a, SIZE);
+    mode = findMode(a, SIZE);
+    cout << "The mode is " << mode << "." << endl;
+
+    return 0;
+}
+
+void getData(int a[], int size)
+{
+    cout << "Enter number of pieces of pie these people eat in a year below." << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Person " << (i + 1) << ": ";
+        cin >> a[i];
+    }
+}
+
+int findMode(int a[], int size)
+{
+    int *b = a; //pointer to the array to compare values to
+    int count[SIZE] = {0}, //count array to hold many times a value occurs in the array
+        modeIndex, 
+        modeValue;
+
+    for (int x = 0; x < size; x++) //for stepping through pointer to int array
+    {
+        for (int i = 0; i < size; i++) //for stepping through int array
+        {
+            if (a[i] == *b)
+                count[x]++;
+        }
+        b++;
+    }
+
+    modeIndex = count[0];
+    modeValue = a[0];
+    for (int i = 1; i < size; i++) 
+    {
+        if (count[i] > modeIndex)
+        {
+            modeIndex = count[i];
+            modeValue = a[i];
+        }
+    }
+
+    return modeValue;
+}
