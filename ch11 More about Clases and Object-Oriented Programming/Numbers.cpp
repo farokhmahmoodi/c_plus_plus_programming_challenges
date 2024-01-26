11402a124fa9b8de35e79f6cd134105a5abd06d0
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-01-26 16:46:19 -0600

    Create Numbers.cpp

---
+++ ch11 More about Clases and Object-Oriented Programming/Numbers.cpp
@@ -0,0 +1,12 @@
+#include "Numbers.h"
+
+//Definitions of static string members
+string Numbers::lessThan20[] = { "zero", "one",
+"two", "three", "four", "five", "six", "seven",
+"eight", "nine", "ten", "eleven", "twelve",
+"thirteen","fourteen", "fifteen", "sixteen",
+"seventeen", "eighteen", "nineteen" };
+
+string Numbers::hundred = "hundred";
+
+string Numbers::thousand = "thousand";
