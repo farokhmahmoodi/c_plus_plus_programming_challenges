04052d90de658b6058f25d931e45fc904b43d3ae
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-01-29 02:47:02 -0600

    Add files via upload

--- ch11 More about Clases and Object-Oriented Programming/Numbers.h
+++ ch11 More about Clases and Object-Oriented Programming/Numbers.h
@@ -17,11 +17,40 @@ public:
 			number = a;
 		else
 			number = 0;
-	}	
+	}
 	void print()
 	{
 		string n = to_string(number); //convert number to string
-		
+		switch (n.size()) //switch statement for how many digits in the number
+		{
+		case 1: //numbers 0-9
+			for (int i = 0; i < 10; i++)
+			{
+				if (number == i)
+				{
+					cout << lessThan20[i] << endl;
+					break;
+				}
+			}
+			break;
+		case 2: //numbers 10-99
+			if (number < 20)
+			{
+				for (int i = 9; i < 20; i++)
+				{
+					if (number == i)
+					{
+						cout << lessThan20[i] << endl;
+						break;
+					}
+				}
+			}
+			else
+			{
+
+			}
+			break;
+		}
 	}
 };
 #endif
