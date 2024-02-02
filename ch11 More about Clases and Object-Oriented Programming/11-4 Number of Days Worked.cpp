a66b9085868f8c460c8a32223ffc6634aaf7ba92
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-02 15:59:04 -0600

    Create 11-4 Number of Days Worked.cpp

---
+++ ch11 More about Clases and Object-Oriented Programming/11-4 Number of Days Worked.cpp
@@ -0,0 +1,25 @@
+/*Design a class called NumDays. The class’s purpose is to store a value that represents a number of work hours and 
+convert it to a number of days. For example, 8 hours would be converted to 1 day, 12 hours would be converted to 1.5 days, 
+and 18 hours would be converted to 2.25 days. The class should have a constructor that accepts a number of hours, as well
+as member functions for storing and retrieving the hours and days. The class should also have the following overloaded 
+operators:
+
+The addition operator +. The number of hours in the sum of two objects is the sum of the number of hours in the individual 
+objects.
+
+The subtraction operator −. The number of hours in the difference of two objects X and Y is the number of hours in X 
+minus the number of hours in Y.
+
+Prefix and postfix increment operators ++. The number of hours in an object is incremented by 1.
+
+Prefix and postfix decrement operators −−. The number of hours in an object is decremented by 1.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
