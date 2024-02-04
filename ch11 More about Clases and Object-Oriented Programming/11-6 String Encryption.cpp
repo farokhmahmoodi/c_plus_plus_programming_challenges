699f0ae39adb34588d73bdc6a31ddc3a9dd7e86f
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-04 14:16:43 -0600

    Add files via upload

--- ch11 More about Clases and Object-Oriented Programming/11-6 String Encryption.cpp
+++ ch11 More about Clases and Object-Oriented Programming/11-6 String Encryption.cpp
@@ -14,13 +14,54 @@ using namespace std;
 
 class EncryptableString : public string
 {
-
-
+private:
+    string plainText,encryptedText;
+public:
+    EncryptableString()
+    {
+        plainText = "";
+    }
+    EncryptableString(string s)
+    {
+        plainText = s;
+    }
+    void encrypt()
+    {
+        for (int i = 0; i < plainText.size(); i++)
+        {
+            if (plainText[i] == 'z')
+                encryptedText += 'a';
+            else if (plainText[i] == 'Z')
+                encryptedText += 'A';
+            else
+                encryptedText += plainText[i] + 1;
+        }
+        cout << "String " << plainText << " encrypted is " << encryptedText << endl;
+    }
 };
 
 int main()
 {
-    
+    int choice;
+    string in;
+
+    do
+    {
+        cout << "Enter a string and the program will encrypt it:";
+        getline(cin, in);
+        EncryptableString test(in);
+        test.encrypt();
+        do
+        {
+            cout << "Would you like to enter another string to encrypt?"
+                << "(1 for yes/2 for no):";
+            cin >> choice;
+            if (choice != 1 && choice != 2)
+                cout << "Invalid input for choice." << endl;
+            cin.ignore();
+        } while (choice != 1 && choice != 2);
+    } while (choice != 2);
 
     return 0;
 }
+
