da9ca39f5d1574f8e99b5a56ada1560c88d2536a
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-03 23:35:53 -0600

    Add files via upload

---
+++ ch11 More about Clases and Object-Oriented Programming/11-6 String Encryption.cpp
@@ -0,0 +1,19 @@
+/*Write a class EncryptableString that is derived from the STL string class. The Encryptable string class adds a member 
+function
+
+void encrypt()
+
+that encrypts the string contained in the object by replacing each letter with its successor in the ASCII ordering. For 
+example, the string baa would be encrypted to cbb. Assume that all characters that are part of an EncryptableString 
+object are letters a, .., z and A, .., Z, and that the successor of z is a and the successor of Z is A. Test your class
+with a program that asks the user to enter strings that are then encrypted and printed.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
+
