f09168f79cea32b1d126879cea85821eacd20c4b
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-03 23:34:16 -0600

    Add files via upload

--- ch11 More about Clases and Object-Oriented Programming/11-5 Palindrome Testing.cpp
+++ ch11 More about Clases and Object-Oriented Programming/11-5 Palindrome Testing.cpp
@@ -12,11 +12,55 @@ You may find it useful to use the subscript operator [] of the string class: If 
 integer, then str[k] returns the character at position k in the string.*/
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Pstring : public string
+{
+private:
+    string a;
+public:
+    Pstring(const string &obj) : string(obj)
+    {
+        a = obj;
+    }
+    bool isPalindrome()
+    {
+        for (int i = 0, x = a.size() - 1; i < a.size(); i++,x--)
+        {
+            if (i == a.size() - 1) //examining last character in string
+                return true;
+            else if (tolower(a[i]) != tolower(a[x]))
+                return false;
+        }
+    }
+};
 
 int main()
 {
-    
+    string s;
+    int choice;
+
+    do
+    {
+        cout << "Enter a string and the program will determine " <<
+            "if it is a palindrome or not:";
+        getline(cin, s);
+        Pstring test(s);
+        if (test.isPalindrome())
+            cout << "String " << s << " is a palindrome." << endl;
+        else
+            cout << "String " << s << " is not a palindrome." << endl;
+        do
+        {
+            cout << "Would you like to run this program again? (1 for yes/2 for no):";
+            cin >> choice;
+            cin.ignore();
+            if (choice != 1 && choice != 2)
+                cout << "Invalid input for choice." << endl;
+        } while (choice != 1 && choice != 2);
+    } while (choice != 2);
     
     return 0;
 }
+
