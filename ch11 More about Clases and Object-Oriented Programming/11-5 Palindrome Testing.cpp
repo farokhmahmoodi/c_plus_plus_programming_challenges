d89f18c6b8239b700a182c89d25830de4b1dbaf2
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-03 22:21:28 -0600

    Add files via upload

---
+++ ch11 More about Clases and Object-Oriented Programming/11-5 Palindrome Testing.cpp
@@ -0,0 +1,22 @@
+/*A palindrome is a string that reads the same backward as forward. For example, the words mom, dad, madam, and 
+radar are all palindromes. Write a class Pstring that is derived from the STL string class. The Pstring class 
+adds a member function
+
+bool isPalindrome( )
+that determines whether the string is a palindrome. Include a constructor that takes an STL string object as parameter 
+and passes it to the string base class constructor. Test your class by having a main program that asks the user to enter 
+a string. The program uses the string to initialize a Pstring object and then calls isPalindrome() to determine whether 
+the string entered is a palindrome.
+
+You may find it useful to use the subscript operator [] of the string class: If str is a string object and k is an 
+integer, then str[k] returns the character at position k in the string.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
