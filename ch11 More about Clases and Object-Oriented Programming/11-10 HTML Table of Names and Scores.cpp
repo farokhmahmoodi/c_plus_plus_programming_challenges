7838f7515fcdb345c820624e1f39c76062f27ea5
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-10 12:42:45 -0600

    Add files via upload

--- ch11 More about Clases and Object-Oriented Programming/11-10 HTML Table of Names and Scores.cpp
+++ ch11 More about Clases and Object-Oriented Programming/11-10 HTML Table of Names and Scores.cpp
@@ -4,7 +4,39 @@ The class should have an overloaded output operator that outputs its data in the
 Make up suitable input and use it to test your class.*/
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
+
+class Student
+{
+private:
+	string name;
+	int score;
+public:
+	Student()
+	{
+		name = "";
+		score = 0;
+	}
+	Student(string n, int s)
+	{
+		name = n;
+		if(score >= 0)
+			score = s;
+		else
+		{
+			cout << "Score cannot be negative." << endl;
+			score = 0;
+		}
+	}
+};
+
+class HTMLTable
+{
+
+};
 
 int main()
 {
