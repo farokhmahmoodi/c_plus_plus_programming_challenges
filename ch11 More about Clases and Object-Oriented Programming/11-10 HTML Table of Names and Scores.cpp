b081c63b8e86e068c1398edb24cec75c16fba8f9
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-10 12:31:56 -0600

    Add files via upload

---
+++ ch11 More about Clases and Object-Oriented Programming/11-10 HTML Table of Names and Scores.cpp
@@ -0,0 +1,13 @@
+/*Write a class whose constructor takes a vector of Student objects, where each Student has a name of type 
+string and a score of type int. The class internally stores the data passed to it in its constructor. 
+The class should have an overloaded output operator that outputs its data in the form of an HTML table. 
+Make up suitable input and use it to test your class.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+	return 0;
+}
