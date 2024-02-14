814c0161e48ea048f2793ae2f923b2d425086c7f
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-14 14:21:43 -0600

    Create 12-2 Average Number of Letters.cpp

---
+++ ch12 More on C-Strings and the string Class/12-2 Average Number of Letters.cpp
@@ -0,0 +1,54 @@
+/*Modify the program you wrote for Problem 1 (Word Counter), so that it also displays the average number of 
+letters in each word.*/
+
+#include <iostream>
+using namespace std;
+
+int numWords(const char*);
+
+int main()
+{
+    const int LENGTH = 201;
+    char choice, * test = nullptr;
+    test = new char[LENGTH];
+
+    do
+    {
+        cout << "Enter a string with no more than 200 characters and the program will determine how many "
+            << "words are in the string:";
+        cin.getline(test, LENGTH);
+        cout << "There are " << numWords(test) << " words in the string "
+            << test << endl;
+        cout << "Would you like to run this program again? (Y for yes/N for no)";
+        cin >> choice;
+        while (toupper(choice) != 'Y' && toupper(choice) != 'N')
+        {
+            cout << "Invalid input for choice." << endl;
+            cout << "Would you like to run this program again? (Y for yes/N for no)";
+            cin >> choice;
+        }
+        cin.ignore();
+    } while (toupper(choice) != 'N');
+
+    return 0;
+}
+
+int numWords(const char* a)
+{
+    int index = 0, count = 0;
+
+    if (a[index] != ' ')
+        count++;
+
+    while (a[index] != '\0')
+    {
+        if (isspace(a[index]))
+        {
+            if (a[index + 1] != ' ' && a[index + 1] != '\0')
+                count++;
+        }
+        index++;
+    }
+
+    return count;
+}
