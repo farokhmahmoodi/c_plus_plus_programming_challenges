2a759877eb4c12ed1816e69559ed2017a327bf7e
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-14 12:01:51 -0600

    Create 12-1 Word Counter.cpp

---
+++ ch12 More on C-Strings and the string Class/12-1 Word Counter.cpp
@@ -0,0 +1,16 @@
+/*Write a function that accepts a C-string as an argument and returns the number of words contained in the string. 
+For instance, if the string argument is “Four score and seven years ago” the function should return the number 6. 
+Demonstrate the function in a program that asks the user to input a string and then passes it to the function. 
+The number of words in the string should be displayed on the screen.*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+
+
+    return 0;
+}
