70aa099cc22f79021f9ef1f50bdab3c3bb56024b
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-15 15:40:38 -0600

    Create 12-4 Vowels and Consonants.cpp

---
+++ ch12 More on C-Strings and the string Class/12-4 Vowels and Consonants.cpp
@@ -0,0 +1,28 @@
+/*Write a function that accepts a C-string as its argument. The function should count the number of vowels appearing in 
+the string and return that number.
+
+Write another function that accepts a C-string as its argument. This function should count the number of consonants 
+appearing in the string and return that number.
+
+Demonstrate the two functions in a program that performs the following steps:
+
+    1.The user is asked to enter a string.
+
+    2.The program displays the following menu:
+        Count the number of vowels in the string
+        Count the number of consonants in the string
+        Count both the vowels and consonants in the string
+        Enter another string
+        Exit the program
+
+    3.The program performs the operation selected by the user and repeats until the user selects E, to exit the program.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
