32ee95679d5f8f561c8b4b72f3121f1cc077f348
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-15 12:27:31 -0600

    Create 12-3 Sentence Capitalizer.cpp

---
+++ ch12 More on C-Strings and the string Class/12-3 Sentence Capitalizer.cpp
@@ -0,0 +1,14 @@
+/*Write a function that accepts a C-string as an argument and capitalizes the first character of each sentence in the 
+string. For instance, if the string argument is “hello. my name is Joe.  what is your name?” the function should manipulate 
+the string so it contains “Hello. My name is Joe. What is your name?” Demonstrate the function in a program that asks the 
+user to input a string and then passes it to the function. The modified string should be displayed on the screen.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
