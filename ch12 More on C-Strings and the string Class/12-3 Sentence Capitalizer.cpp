4c315dadae9d885f776e74fd36eb0eb8a45cd6ac
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-15 14:41:45 -0600

    Update 12-3 Sentence Capitalizer.cpp

--- ch12 More on C-Strings and the string Class/12-3 Sentence Capitalizer.cpp
+++ ch12 More on C-Strings and the string Class/12-3 Sentence Capitalizer.cpp
@@ -6,9 +6,38 @@ user to input a string and then passes it to the function. The modified string s
 #include <iostream>
 using namespace std;
 
+void sentenceCapitalizer(char*, char*);
+
 int main()
 {
-    
+    const int LENGTH = 100;
+    char* a = nullptr, * b = nullptr;
+    a = new char[LENGTH], b = new char[LENGTH];
+
+    cout << "Enter a string and the program will capitalize "
+        << "the first character of each sentence in the string:";
+    cin.getline(a, LENGTH);
+    sentenceCapitalizer(a, b);
 
     return 0;
+}
+
+void sentenceCapitalizer(char* a, char* b)
+{
+    int index = 0;
+    
+    strcpy(b, a);
+    if (a[index] != ' ')
+        b[index] = toupper(a[index]);
+
+    while (a[index] != '\0')
+    {
+        if (ispunct(a[index]))
+        {
+            if (a[index + 1] != ' ')
+                b[index + 1] = toupper(a[index + 1]);
+        }
+        index++;
+    }
+    cout << "The modified string:" << b << endl;
 }
