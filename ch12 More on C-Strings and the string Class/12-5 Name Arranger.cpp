f31c31fa06dde83de27457affb3e329971a81eb8
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-16 10:49:08 -0600

    Update 12-5 Name Arranger.cpp

--- ch12 More on C-Strings and the string Class/12-5 Name Arranger.cpp
+++ ch12 More on C-Strings and the string Class/12-5 Name Arranger.cpp
@@ -7,9 +7,34 @@ Display the contents of the fourth array on the screen.*/
 #include <iostream>
 using namespace std;
 
+void nameArranger(const char*, const char*, const char*);
+
 int main()
 {
-    
+    const int LENGTH = 21;
+    char* firstName = new char[LENGTH],
+        * middleName = new char[LENGTH],
+        * lastName = new char[LENGTH];
+
+    cout << "Enter first name no longer than " << LENGTH - 1
+        << " characters:";
+    cin.getline(firstName, LENGTH);
+    cout << "Enter middle name no longer than " << LENGTH - 1
+        << " characters:";
+    cin.getline(middleName, LENGTH);
+    cout << "Enter last name no longer than " << LENGTH - 1
+        << " characters:";
+    cin.getline(lastName, LENGTH);
+
+
 
     return 0;
+}
+
+void nameArranger(const char* first, const char* middle, const char* last)
+{
+    char* arranger = nullptr;
+
+
+
 }
