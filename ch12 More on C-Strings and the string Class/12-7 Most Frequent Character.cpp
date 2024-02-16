0ed984ab5bae1336971ad662aa44673162acd9dd
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-16 16:01:19 -0600

    Create 12-7 Most Frequent Character.cpp

---
+++ ch12 More on C-Strings and the string Class/12-7 Most Frequent Character.cpp
@@ -0,0 +1,13 @@
+/*Write a function that accepts either a pointer to a C-string or a string object as its argument. 
+The function should return the character that appears most frequently in the string. 
+Demonstrate the function in a complete program.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
