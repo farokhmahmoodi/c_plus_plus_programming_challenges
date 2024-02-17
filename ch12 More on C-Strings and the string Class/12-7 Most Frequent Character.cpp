6c48803d8707f45df7a0078032d54b4f6a63c71a
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-17 17:59:51 -0600

    Add files via upload

--- ch12 More on C-Strings and the string Class/12-7 Most Frequent Character.cpp
+++ ch12 More on C-Strings and the string Class/12-7 Most Frequent Character.cpp
@@ -12,18 +12,45 @@ void swap(char& a, char& b);
 int main()
 {
     const int LENGTH = 101;
-    char *a = new char[LENGTH];
+    char *a = new char[LENGTH], *sorted = new char[LENGTH];
 
-
+    cout << "Enter a string no greater than "
+        << LENGTH - 1 << " characters:";
+    cin.getline(a, LENGTH);
+    strcpy(sorted, a);
+    selectionSort(sorted);
+    cout << "The most frequent character in the string is "
+        << mostFrequent(sorted) << endl;
 
     return 0;
 }
 
 char mostFrequent(const char* a)
 {
-    int index = 0;
+    int index = 0, mode = 0, hold = 0;
+    char most, current;
+    most = current = a[index];
 
+    while (isprint(a[index]))
+    {
+        if (a[index] != current)
+        {
+            current = a[index];
+            mode = 0;
+        }
+        mode++;
+        if (mode > hold)
+        {
+            hold = mode;
+            most = current;
+        }
+        index++;
+        cout << "current:" << current << endl;
+        cout << "most:" << most << endl;
+        cout << "mode:" << mode << endl << endl;
+    }
 
+    return most;
 }
 
 void selectionSort(char* a)
