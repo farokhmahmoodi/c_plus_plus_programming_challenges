fcba75080a529a840a53280262a9ae9184e96529
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-18 13:56:24 -0600

    Add files via upload

--- ch12 More on C-Strings and the string Class/12-11 Phone Number List.cpp
+++ ch12 More on C-Strings and the string Class/12-11 Phone Number List.cpp
@@ -21,11 +21,32 @@ Geri Palmer, 223–8787
 Ron Palmer, 486–2783*/
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+void searchName(const string[],string);
 
 int main()
 {
-    
+    string a[11] = {"Hoshikawa Tanaka, 678–1223", "Joe Looney, 586–0097","Geri Palmer, 223–8787",
+    "Lynn Lopez, 887–1212","Holly Gaddis, 223–8878","Sam Wiggins, 486–0998","Bob Kain, 586–8712" ,
+    "Tim Haynes, 586–7676","Warren Gaddis, 223–9037","Jean James, 678–4939","Ron Palmer, 486–2783"};
+    string value;
+
+    for (int i = 0; i < 11; i++)
+        cout << a[i] << endl;
+    cout << "Enter a name or partial name to search for in list above:";
+    getline(cin, value);
+    searchName(a, value);
 
     return 0;
+}
+
+void searchName(const string a[], string value)
+{
+    for (int i = 0; i < a->size(); i++)
+    {
+        if (a[i].find(value) != -1) //if find function does not return static member string::npos of thestring class
+            cout << a[i] << endl;
+    }
 }
