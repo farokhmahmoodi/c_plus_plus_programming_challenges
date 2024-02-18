2b75015d4d520acb8fabcef484f1f801c6f72385
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-17 18:32:00 -0600

    Add files via upload

--- ch12 More on C-Strings and the string Class/12-8 replaceSubstring Function.cpp
+++ ch12 More on C-Strings and the string Class/12-8 replaceSubstring Function.cpp
@@ -13,10 +13,32 @@ that fence”. Demonstrate the function in a complete program.*/
 #include <iostream>
 using namespace std;
 
+string replaceSubstring(char*, const char*, const char*);
+
 int main()
 {
+    const int LENGTH = 101;
+    char* string1 = new char[LENGTH],
+        *string2 = new char[LENGTH], *string3 = new char[LENGTH];
     
+    cin.getline(string1, LENGTH);
+    cin.getline(string2, LENGTH);
+    cin.getline(string3, LENGTH);
+    replaceSubstring(string1, string2, string3);
 
     return 0;
 }
 
+string replaceSubstring(char* string1, const char* string2, const char* string3)
+{
+    int index = 0;
+
+    while (isprint(string1[index]) && string1[index] != '\0')
+    {
+
+
+        index++;
+    }
+
+    return string1;
+}
