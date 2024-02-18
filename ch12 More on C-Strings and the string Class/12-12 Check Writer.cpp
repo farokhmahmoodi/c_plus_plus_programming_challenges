4338d83deee0caeca847786eea51b003fe47595d
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-18 13:58:01 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-12 Check Writer.cpp
@@ -0,0 +1,22 @@
+/*Write a program that displays a simulated paycheck. The program should ask the user to enter the date, 
+the payee’s name, and the amount of the check. It should then display a simulated check with the dollar 
+amount spelled out, as shown here:
+
+                                 Date: 12/24/2016
+Pay to the Order of: John Phillips           $1920.85
+One thousand nine hundred twenty and 85 cents
+
+You may assume the amount is no greater than $10000. Be sure to format the numeric value of the check in 
+fixed-point notation with two decimal places of precision. Be sure the decimal place always displays, 
+even when the number is zero or has no fractional part. Use either C-strings or string class objects in 
+this program.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
