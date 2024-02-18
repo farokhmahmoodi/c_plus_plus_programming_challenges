95a3acd071b30949b9d2d0bffb617b68a6e1a267
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-18 13:10:44 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-10 Password Verifier.cpp
@@ -0,0 +1,22 @@
+/*Imagine you are developing a software package that requires users to enter their own passwords. 
+Your software requires that user’s passwords meet the following criteria:
+
+The password should be at least six characters long.
+
+The password should contain at least one uppercase and at least one lowercase letter.
+
+The password should have at least one digit.
+
+Write a program that asks for a password and then verifies that it meets the stated criteria. 
+If it doesn’t, the program should display a message telling the user why.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
+
