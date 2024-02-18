2fba0b7926dfbb4dd05eeb8e4e18482521eeaa02
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-17 19:49:50 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-9 Case Manipulator.cpp
@@ -0,0 +1,22 @@
+/*Write a program with three functions: upper, lower, and flip. The upper function should accept a C-string 
+as an argument. It should step through all the characters in the string, converting each to uppercase. 
+The lower function, too, should accept a pointer to a C-string as an argument. It should step through all 
+the characters in the string, converting each to lowercase. Like upper and lower, flip should also accept a 
+C-string. As it steps through the string, it should test each character to determine whether it is upper- or 
+lowercase. If a character is uppercase, it should be converted to lowercase. If a character is lowercase, it 
+should be converted to uppercase.
+
+Test the functions by asking for a string in function main, then passing it to them in the following order: 
+flip, lower, and upper.*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+
+    return 0;
+}
