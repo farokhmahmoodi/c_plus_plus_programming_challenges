7bc4ed3cf76c9539d7a7837e8e71d15e3787a0d7
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-22 10:13:05 -0600

    Create 12-16 Pig Latin.cpp

---
+++ ch12 More on C-Strings and the string Class/12-16 Pig Latin.cpp
@@ -0,0 +1,17 @@
+/*Write a program that reads a sentence as input and converts each word to “Pig Latin.” In one version, 
+to convert a word to Pig Latin you remove the first letter and place that letter at the end of the word. 
+Then you append the string “ay” to the word. Here is an example:
+
+English: I SLEPT MOST OF THE NIGHT
+
+Pig Latin: IAY LEPTSAY OSTMAY FOAY HETAY IGHTNAY*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
