861ab09c443e0c92dbb5d6bdb8ead04504de1588
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-22 09:10:37 -0600

    Update 12-14 Dollar Amount Formatter.cpp

--- ch12 More on C-Strings and the string Class/12-14 Dollar Amount Formatter.cpp
+++ ch12 More on C-Strings and the string Class/12-14 Dollar Amount Formatter.cpp
@@ -2,41 +2,47 @@
 
 string dollarFormat(double amount)
 
-that takes a dollar amount in numeric form and returns a string formatted in currency notation, with a $ 
+that takes a dollar amount in numeric form and returns a string formatted in currency notation, with a $
 sign and commas inserted at the appropriate locations. Test your function using suitable inputs.*/
-    
+
 #include <iostream>
+#include <sstream>
 #include <string>
 using namespace std;
 
-string dollarFormat(string);  // Prototype
+string dollarFormat(double);
 
 int main(void)
 {
-    string input;    // User input
+    double amount;
 
     // Get the dollar amount from the user
     cout << "Enter a dollar amount in the form nnnnn.nn : ";
-    cin >> input;
+    cin >> amount;
 
     // Display the formatted dollar amount
-    cout << "Formatted amount:   " << dollarFormat(input) << endl;
+    cout << "Formatted amount:   " << dollarFormat(amount) << endl;
     return 0;
 }
 
-//******************************************************
-// Returns a $-formatted version of the input string.  *
-//******************************************************
-string dollarFormat(string original)
+string dollarFormat(double amount)
 {
-    string formatted = original;
-    int dp = formatted.find('.');  // Position of decimal point
-    int pos = dp;                  // Search for comma position
+    ostringstream ostr;
+    string str;
+    ostr << amount;
+    string amt = ostr.str();
+    istringstream istr(amt);
+    istr >> str;
+    int dp = str.find('.');  // Position of decimal point
+    int pos = dp;            // Search for comma position
+
     while (pos > 3)
     {
         pos = pos - 3;
-        formatted.insert(pos, ",");
+        str.insert(pos, ",");
     }
-    formatted.insert(0, "$");
-    return formatted;
+    str.insert(0, "$");
+
+
+    return str;
 }
