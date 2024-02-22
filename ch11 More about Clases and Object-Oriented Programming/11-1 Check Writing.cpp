1292024dfd45e1ba7c64f6940144f377be02b472
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-21 22:28:47 -0600

    Add files via upload

--- ch11 More about Clases and Object-Oriented Programming/11-1 Check Writing.cpp
+++ ch11 More about Clases and Object-Oriented Programming/11-1 Check Writing.cpp
@@ -1,22 +1,22 @@
-/*Design a class Numbers that can be used to translate whole dollar amounts in the range 0 through 9999 into an English 
-description of the number. For example, the number 713 would be translated into the string seven hundred thirteen, and 
+/*Design a class Numbers that can be used to translate whole dollar amounts in the range 0 through 9999 into an English
+description of the number. For example, the number 713 would be translated into the string seven hundred thirteen, and
 8203 would be translated into eight thousand two hundred three.
 
 The class should have a single integer member variable
 
 int number;
 
-and a collection of static string members that specify how to translate key dollar amounts into the desired format. 
+and a collection of static string members that specify how to translate key dollar amounts into the desired format.
 For example, you might use static strings such as
 
 string lessThan20[ ] =
-    {"zero", "one", â€¦, "eighteen", "nineteen" };
+    {"zero", "one", …, "eighteen", "nineteen" };
 string hundred = "hundred";
 string thousand = "thousand";
 
-The class should have a constructor that accepts a non-negative integer and uses it to initialize the Numbers object. 
-It should have a member function print() that prints the English description of the Numbers object. Demonstrate the class 
-by writing a main program that asks the user to enter a number in the proper range and then prints out its English 
+The class should have a constructor that accepts a non-negative integer and uses it to initialize the Numbers object.
+It should have a member function print() that prints the English description of the Numbers object. Demonstrate the class
+by writing a main program that asks the user to enter a number in the proper range and then prints out its English
 description.*/
 
 #include "Numbers.h"
@@ -34,15 +34,15 @@ int main()
                 cout << "Input error. Number must be between 0 and 9999." << endl;
         } while (num < 0 || num > 9999);
         Numbers a(num);
-        a.print();
-		do
-		{
-			cout << "Would you like to enter another number? (1 for yes/2 for no)?";
-			cin >> choice;	
-			if(choice != 1 && choice != 2)
-				cout << "Invalid input for choice." << endl;
-		}while(choice != 1 && choice != 2);
+        cout << a.print() << endl;
+        do
+        {
+            cout << "Would you like to enter another number? (1 for yes/2 for no)?";
+            cin >> choice;
+            if (choice != 1 && choice != 2)
+                cout << "Invalid input for choice." << endl;
+        } while (choice != 1 && choice != 2);
     } while (choice != 2);
-   
+
     return 0;
 }
--- ch11 More about Clases and Object-Oriented Programming/Numbers.cpp
+++ ch11 More about Clases and Object-Oriented Programming/Numbers.cpp
@@ -1,18 +1,10 @@
 #include "Numbers.h"
 
 //Definitions of static string members
-string Numbers::lessThan20[] = { "zero", "one",
+string Numbers::convert[] = { "zero", "one",
 "two", "three", "four", "five", "six", "seven",
 "eight", "nine", "ten", "eleven", "twelve",
 "thirteen","fourteen", "fifteen", "sixteen",
-"seventeen", "eighteen", "nineteen" };
-string Numbers::twenty = "twenty";
-string Numbers::thirty = "thirty";
-string Numbers::forty = "forty";
-string Numbers::fifty = "fifty";
-string Numbers::sixty = "sixty";
-string Numbers::seventy = "seventy";
-string Numbers::eighty = "eighty";
-string Numbers::ninety = "ninety";
-string Numbers::hundred = "hundred";
-string Numbers::thousand = "thousand";
+"seventeen", "eighteen", "nineteen", "twenty",
+"thirty", "forty", "fifty", "sixty", "seventy",
+"eighty", "ninety", "hundred", "thousand" };
