c5eafbcec68796ffe3a8aeb69986894b82e4a5ec
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-22 09:19:56 -0600

    Create 12-15 Word Separator.cpp

---
+++ ch12 More on C-Strings and the string Class/12-15 Word Separator.cpp
@@ -0,0 +1,14 @@
+/*Write a program that accepts as input a sentence in which all of the words are run together, but the first character 
+of each word is uppercase. Convert the sentence to a string in which the words are separated by spaces and only the 
+first word starts with an uppercase letter. For example the string “StopAndSmellTheRoses.” would be converted to “Stop and 
+smell the roses.”*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
