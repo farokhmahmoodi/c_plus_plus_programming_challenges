fa50e4f9e639be66cfb4d7b55152e6f1e7b44a6c
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-21 22:35:01 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-13 Digit Sums of Squares and Cubes.cpp
@@ -0,0 +1,14 @@
+/*If you add up all the digits in 468, you get 4 + 6 + 8 = 18. The square and cube of 468 are 219024 and 
+102503232, respectively. Interestingly, if you add up the digits of the square or cube, you get 18 again. 
+Are there other integers that share this property? Write a program that lists all positive integers k less 
+than 1000 such that the three numbers k, k^2, and k^3 have digits that add up to the same number.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
