9720988ddcab368fcc971efff92d633bf27ebff1
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-23 16:41:12 -0600

    Create 12-17 I before e except after c.cpp

---
+++ ch12 More on C-Strings and the string Class/12-17 I before e except after c.cpp
@@ -0,0 +1,14 @@
+/*A friend of yours who is an educator is conducting research into the effectiveness of teaching the spelling rule 
+“I before e except after c” to students. She wishes to analyze writing samples from two groups of students, only one 
+of which was taught the rule. Write a program that will take a file containing a writing sample and print a list of 
+all words in the file that contain at least one of the strings “ie” or “ei”.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
