03ebbd5694e54bcd4a4050c919fbf6f7314e780e
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 15:14:08 -0600

    Add files via upload

--- ch12 More on C-Strings and the string Class/12-18 User Name.cpp
+++ ch12 More on C-Strings and the string Class/12-18 User Name.cpp
@@ -5,11 +5,30 @@ Hello, gcm
 when it is executed.*/
 
 #include <iostream>
+#include <sstream>
+#include <string>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    
+    extern char** environ; //Needed to access the environment
+    ostringstream ostr;
+    int k = 0;
+
+    while (environ[k] != 0) //querying environment
+    {
+        istringstream istr(environ[k]);
+        string temp;
+        istr >> temp;
+        if (temp.find("USERNAME=", 0) != -1)
+        {
+            temp.erase(temp.find("USERNAME=", 0), 9);
+            ostr << "Hello, " << temp << endl;
+            break;
+        }
+        k++;
+    }
+    cout << ostr.str();
 
     return 0;
 }
