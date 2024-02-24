680b077d17aca261ecc886c946a50d06a811f6c0
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 13:02:26 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-18 User Name.cpp
@@ -0,0 +1,16 @@
+/*Write a program that queries its environment, determines the user’s login name, and then greets the user by name. 
+For example, if the login name of the user is gcm, then the program prints
+
+Hello, gcm
+when it is executed.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
+
