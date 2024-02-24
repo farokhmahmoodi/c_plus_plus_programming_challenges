58cbad32cb1b09b62999d5bffd621d1847d7ec37
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 15:16:08 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-19 String Splitter.cpp
@@ -0,0 +1,25 @@
+/*Write a function vector<string> split(string str) that takes a string as parameter and returns a vector of the distinct 
+words in the string. A distinct word is any run of characters delimited by the beginning of the string, the end of the 
+string, or whitespace. A consecutive run of whitespace characters is to be treated the same as a single whitespace 
+character. Test your program by having the user enter lines of input, reading and splitting the line into words, and 
+printing the words in the returned vector with each word on its own line. For example, the input
+
+Every       good boy      does fine.
+
+results in the output
+
+Every
+good
+boy 
+does
+fine.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
