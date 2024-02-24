a71a1f058db65e15efd32f28038363f1a403b5f4
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 15:32:57 -0600

    Add files via upload

--- ch12 More on C-Strings and the string Class/12-19 String Splitter.cpp
+++ ch12 More on C-Strings and the string Class/12-19 String Splitter.cpp
@@ -15,11 +15,61 @@ does
 fine.*/
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
+
+vector<string> split(string);
 
 int main()
 {
-    
+    string input;
+    vector<string> splitter;
+    char choice;
+
+    do
+    {
+        cout << "Enter a string and the program will print each word in the string on its own line:";
+        getline(cin, input);
+        splitter = split(input);
+        for (int i = 0; i < splitter.size(); i++)
+        {
+            cout << splitter[i];
+        }
+        do
+        {
+            cout << "Would you like to enter another string? (Y for yes/N for no)";
+            cin >> choice;
+            if (toupper(choice) != 'Y' && toupper(choice) != 'N')
+                cout << "Invalid input for choice." << endl;
+        } while (toupper(choice) != 'Y' && toupper(choice) != 'N');
+        cin.ignore();
+    } while (toupper(choice) != 'N');
 
     return 0;
+}
+
+vector<string> split(string str)
+{
+    istringstream istr(str), istr2(str);
+    int wordCount = 0;
+    string word;
+    vector<string> spl;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (istr >> word)
+        {
+            wordCount++;
+        }       
+    }
+    for (int i = 0; i < wordCount; i++)
+    {
+        istr2 >> word;
+        spl.push_back(word);
+        spl.push_back("\n");
+    }
+
+    return spl;
 }
