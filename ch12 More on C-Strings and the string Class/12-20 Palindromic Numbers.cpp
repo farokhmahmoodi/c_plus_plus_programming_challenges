4e656b44eed752773c5ac144dbc7507bb255a0ec
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 16:08:35 -0600

    Add files via upload

--- ch12 More on C-Strings and the string Class/12-20 Palindromic Numbers.cpp
+++ ch12 More on C-Strings and the string Class/12-20 Palindromic Numbers.cpp
@@ -18,13 +18,41 @@ reverse(str.begin(), str.end());*/
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
-
+void palindrome(int);
 
 int main()
 {
-   
+	for (int i = 1; i < 1000; i++)
+	{
+		palindrome(i);
+	}
 
 	return 0;
+}
+
+void palindrome(int n)
+{
+	ostringstream ostr, ostr2, palin;
+	istringstream istr, istr2, istr3, istr4;
+	string num, numReversed, square, squareReversed;
+
+	ostr << n;
+	istr.str(ostr.str()), istr2.str(ostr.str());
+	istr >> num, istr2 >> numReversed;
+	reverse(numReversed.begin(), numReversed.end());
+	if (num == numReversed) //if integer is palindrome
+	{
+		ostr2 << n * n;
+		istr3.str(ostr2.str()), istr4.str(ostr2.str());
+		istr3 >> square, istr4 >> squareReversed;
+		reverse(squareReversed.begin(), squareReversed.end());
+		if (square == squareReversed) //if integer squared is palindrome
+		{
+			palin << num << " has square " << square << endl;
+			cout << palin.str();
+		}
+	}
 }
