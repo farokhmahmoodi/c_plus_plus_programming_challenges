5891c664fa8ea4df106a56bc6bf0a7bdccfe670b
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 15:34:13 -0600

    Add files via upload

---
+++ ch12 More on C-Strings and the string Class/12-20 Palindromic Numbers.cpp
@@ -0,0 +1,25 @@
+/*A palindromic number is a positive integer that reads the same forward as backward. For example, the numbers 1, 11, and 
+121 are palindromic. Moreover, 1 and 11 are very special palindromic numbers: their squares are also palindromic. How many 
+positive integers less than 10,000 have this property? Write a program to list all such numbers together with their squares.
+
+The beginning part of your output should look like this:
+
+1 has square 1
+2 has square 4
+3 has square 9
+11 has square 121
+22 has square 484
+
+Hint: If str is a string object, the reverse() function (declared in <algorithm> header) will reverse the string. 
+The code to do that is:
+
+reverse(str.begin(), str.end());*/
+
+#include <iostream>
+
+int main()
+{
+   
+
+	return 0;
+}
