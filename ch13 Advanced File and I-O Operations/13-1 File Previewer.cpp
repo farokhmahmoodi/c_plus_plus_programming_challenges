2af107364e5cf2641997f5a6431dc6dafe99c177
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-02 19:42:41 -0600

    Add files via upload

--- ch13 Advanced File and I-O Operations/13-1 File Previewer.cpp
+++ ch13 Advanced File and I-O Operations/13-1 File Previewer.cpp
@@ -3,11 +3,38 @@ lines of the file on the screen. If the file has fewer than 10 lines, the entire
 along with a message indicating the entire file has been displayed.*/
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    fstream f;
+    string filename, input;
+    int numOfLines = 0;
+
+    cout << "Enter name of a text file:";
+    getline(cin,filename);
+    f.open(filename, ios::in);
+    if (!f)
+    {
+        cout << "Error. Failed to open file." << endl;
+        return 0;
+    }
+    while (numOfLines < 10)
+    {
+        if (getline(f, input))
+        {
+            cout << input << endl;
+        }
+        else
+        {
+            cout << "Entire file has been displayed." << endl;
+            break;
+        }
+        numOfLines++;
+    }
+    f.close();
 
     return 0;
 }
