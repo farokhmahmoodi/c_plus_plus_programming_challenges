d157d237f3a47eaa244af23defea4aa4dee44562
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-03 12:34:46 -0600

    Add files via upload

--- ch13 Advanced File and I-O Operations/13-2 File Display Program.cpp
+++ ch13 Advanced File and I-O Operations/13-2 File Display Program.cpp
@@ -5,11 +5,36 @@ before the next 24 lines are displayed.*/
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    fstream f;
+    string filename, input;
+    char next;
+    int lines = 0;
+
+    cout << "Enter name of file:";
+    getline(cin, filename);
+    f.open(filename, ios::in | ios::out | ios::app);
+    if (!f)
+    {
+        cout << "File failed to open." << endl;
+        return 0;
+    }
+    while (getline(f, input))
+    {
+        cout << input << endl;
+        lines++;
+        if (lines == 24)
+        {
+            system("pause");
+            cin.ignore();
+            lines = 0;
+        }
+    }
+    f.close();
 
     return 0;
 }
