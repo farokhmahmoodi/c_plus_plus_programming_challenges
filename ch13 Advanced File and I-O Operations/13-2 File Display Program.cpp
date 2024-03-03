d465a5c608c7bc595cc06ba50951da751ce607f7
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-02 19:44:13 -0600

    Add files via upload

---
+++ ch13 Advanced File and I-O Operations/13-2 File Display Program.cpp
@@ -0,0 +1,15 @@
+/*Write a program that asks the user for the name of a file. The program should display the contents of the 
+file on the screen. If the file’s contents won’t fit on a single screen, the program should display 24 lines 
+of output at a time and then pause. Each time the program pauses, it should wait for the user to type a key 
+before the next 24 lines are displayed.*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
