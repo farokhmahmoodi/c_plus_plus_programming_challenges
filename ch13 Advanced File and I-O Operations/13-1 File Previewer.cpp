c6f2365c0dde6e60eb1b112a9aba181d6fdf3dfb
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-02 19:09:39 -0600

    Add files via upload

---
+++ ch13 Advanced File and I-O Operations/13-1 File Previewer.cpp
@@ -0,0 +1,14 @@
+/*Write a program that asks the user for the name of a text file. The program should display the first 10 
+lines of the file on the screen. If the file has fewer than 10 lines, the entire file should be displayed 
+along with a message indicating the entire file has been displayed.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
+
