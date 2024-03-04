4792965fc336e54cde56fe4f5b1ee356a5483814
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-04 15:45:16 -0600

    Create 13-4 Tails of a File.cpp

---
+++ ch13 Advanced File and I-O Operations/13-4 Tails of a File.cpp
@@ -0,0 +1,15 @@
+/*Write a program that asks the user for the name of a text file. The program should display the 
+last 10 lines of the file on the screen (the “tail” of the file). If the file has less than 10 lines, 
+the entire file is displayed, with a message that the entire file has been displayed. The program should do 
+this by seeking to the end of the file and then backing up to the tenth line from the end.*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
