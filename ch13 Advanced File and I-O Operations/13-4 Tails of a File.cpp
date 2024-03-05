8075396248965ddebb8c8b5b661c5f8acbc7a6cd
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-04 20:22:01 -0600

    Add files via upload

--- ch13 Advanced File and I-O Operations/13-4 Tails of a File.cpp
+++ ch13 Advanced File and I-O Operations/13-4 Tails of a File.cpp
@@ -1,6 +1,6 @@
-/*Write a program that asks the user for the name of a text file. The program should display the 
-last 10 lines of the file on the screen (the ‚Äútail‚Äù of the file). If the file has less than 10 lines, 
-the entire file is displayed, with a message that the entire file has been displayed. The program should do 
+/*Write a program that asks the user for the name of a text file. The program should display the
+last 10 lines of the file on the screen (the ìtailî of the file). If the file has less than 10 lines,
+the entire file is displayed, with a message that the entire file has been displayed. The program should do
 this by seeking to the end of the file and then backing up to the tenth line from the end.*/
 
 #include <iostream>
@@ -15,6 +15,7 @@ int main()
     char ch;
     long position = 0;
     int lines = 0;
+    bool entire = false;
 
     cout << "Enter file name:";
     getline(cin, filename);
@@ -25,19 +26,30 @@ int main()
         return 0;
     }
     file.seekg(0L, ios::end);
-    while (file.seekg(--position, ios::end) && lines <= 10)
+    while (file.seekg(--position, ios::end))
     {
         ch = file.get();
-        if (ch == '\n' && file.peek() != EOF && file.peek() != '\n')
+        if (ch == '\n' && file.peek() != EOF)
         {
             lines++;
         }
+        if (file.tellg() == 0)
+        {
+            entire = true;
+            break;
+        }
+        if (lines == 10)
+        {
+            break;
+        }
         file.clear();
     }
-    while (getline(file,input))
+    while (getline(file, input))
     {
         cout << input << endl;
     }
+    if (entire)
+        cout << "Entire file has been displayed." << endl;
 
     return 0;
 }
