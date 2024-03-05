0ea827b4a6ccf2c5fd88157da8204a69e6f6c2b0
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-04 20:28:35 -0600

    Add files via upload

---
+++ ch13 Advanced File and I-O Operations/13-5 String Search.cpp
@@ -0,0 +1,16 @@
+/*Write a program that asks the user for the name of a file and a string to search for. The program will 
+search the file for all occurrences of the specified string and display all lines that contain the string. 
+After all occurrences have been located, the program should report the number of times the string appeared in 
+the file.*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
