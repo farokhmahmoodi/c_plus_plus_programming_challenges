dbe94b088e531ce72b7550bd3171c91e0b9d029c
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-07 12:53:06 -0600

    Update 13-5 String Search.cpp

--- ch13 Advanced File and I-O Operations/13-5 String Search.cpp
+++ ch13 Advanced File and I-O Operations/13-5 String Search.cpp
@@ -1,19 +1,18 @@
-/*Write a program that asks the user for the name of a file and a string to search for. The program will 
-search the file for all occurrences of the specified string and display all lines that contain the string. 
-After all occurrences have been located, the program should report the number of times the string appeared in 
+/*Write a program that asks the user for the name of a file and a string to search for. The program will
+search the file for all occurrences of the specified string and display all lines that contain the string.
+After all occurrences have been located, the program should report the number of times the string appeared in
 the file.*/
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 using namespace std;
 
 int main()
 {
     fstream file;
     string filename, input, search, temp;
-    int count = 0;
+    int count = 0, pos = 0;
 
     cout << "Enter filename:";
     getline(cin, filename);
@@ -25,7 +24,22 @@ int main()
     }
     cout << "Enter a string to search for in the file:";
     getline(cin, search);
-
+    while (getline(file, input))
+    {
+        pos = input.find(search, 0);
+        if (pos != -1)
+        {
+            cout << input << endl;
+            count++;
+            pos = input.find(search, ++pos);
+            while (pos != -1)
+            {
+                count++;
+                pos = input.find(search, ++pos);
+            }
+        }
+    }
+    cout << endl;
     cout << "The string occured " << count << " times in the file." << endl;
 
     return 0;
