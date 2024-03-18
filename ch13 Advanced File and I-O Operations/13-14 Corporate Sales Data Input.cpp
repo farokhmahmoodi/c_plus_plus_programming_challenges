a3f801d9dbb0f77f26c67eec8194e2403474c298
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-18 16:03:04 -0500

    Create 13-14 Corporate Sales Data Input.cpp

---
+++ ch13 Advanced File and I-O Operations/13-14 Corporate Sales Data Input.cpp
@@ -0,0 +1,21 @@
+/*Write a program that reads the information in the file created by the program in Programming Challenge 13. 
+The program should calculate and display the following figures:
+
+Total corporate sales for each quarter
+
+Total yearly sales for each division
+
+Total yearly corporate sales
+
+Average quarterly sales for the divisions
+
+The highest and lowest quarters for the corporation*/
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    
+    return 0;
+}
