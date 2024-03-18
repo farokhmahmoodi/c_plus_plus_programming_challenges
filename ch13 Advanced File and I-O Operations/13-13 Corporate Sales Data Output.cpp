5f78b8d84fd1447a2fa28fa721b3c6061eb2f7c4
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-18 00:20:02 -0500

    Add files via upload

---
+++ ch13 Advanced File and I-O Operations/13-13 Corporate Sales Data Output.cpp
@@ -0,0 +1,21 @@
+/*Write a program that uses a structure to store the following information on a company division:
+
+Division name (such as East, West, North, or South)
+
+Quarter (1, 2, 3, or 4)
+
+Quarterly sales
+
+The user should be asked for the four quarters’ sales figures for the East, West, North, and South divisions. The information
+for each quarter for each division should be written to a file.*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
