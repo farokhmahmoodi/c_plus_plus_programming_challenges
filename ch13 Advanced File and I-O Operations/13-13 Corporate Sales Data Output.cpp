fb75bf8eeef296441317aa3856d2b940e0c4dde5
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-18 15:19:01 -0500

    Update 13-13 Corporate Sales Data Output.cpp

--- ch13 Advanced File and I-O Operations/13-13 Corporate Sales Data Output.cpp
+++ ch13 Advanced File and I-O Operations/13-13 Corporate Sales Data Output.cpp
@@ -20,7 +20,7 @@ const int SALES_LENGTH = 16;
 struct Company {
     static string name[DIVISION_LENGTH];
     static int quarter[DIVISION_LENGTH];
-    double sales[SALES_LENGTH];
+    double sales[DIVISION_LENGTH] = { 0 };
 };
 
 string Company::name[DIVISION_LENGTH] = {"East", "West", "North", "South"};
@@ -29,33 +29,42 @@ int Company::quarter[DIVISION_LENGTH] = { 1,2,3,4 };
 int main()
 {
     Company a;
-    fstream file("13-12.dat", ios::out | ios::binary);
+    fstream file("13-12.dat", ios::in | ios::out | ios::binary);
 
     if (!file)
     {
         cout << "File failed to open." << endl;
         return 0;
     }
-    for (int i = 0; i < DIVISION_LENGTH; i++)
+    for (int i = 0, y = 0; i < DIVISION_LENGTH; i++)
     {
         for (int x = 0; x < DIVISION_LENGTH; x++)
-        {
-            for (int y = 0; y < DIVISION_LENGTH; y++)
+        {          
+            do
             {
-                do
-                {
-                    while (cout << "Enter sales for quarter " <<
-                        x + 1 << " for " << a.name[i] << " Division:"
-                        && !(cin >> a.sales[y])) {
-                        cin.clear(); //clear bad input flag
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
-                        cout << "Invalid input for sales." << endl;
-                    }
-                } while (a.sales[y] < 0);
-            }
+                while (cout << "Enter sales for quarter " <<
+                    a.quarter[x] << " for " << a.name[i] << " Division:"
+                    && !(cin >> a.sales[y])) {
+                    cin.clear(); //clear bad input flag
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                    cout << "Invalid input for sales." << endl;
+                }
+            } while (a.sales[y] < 0);
+            y++;
         }
     }
     file.write(reinterpret_cast<char*>(&a), sizeof(a));
+    //file.read(reinterpret_cast<char*>(&a), sizeof(a));
+    //for (int i = 0, y = 0; i < DIVISION_LENGTH; i++)
+    //{
+    //    for (int x = 0; x < DIVISION_LENGTH; x++)
+    //    {
+    //        cout << "Sales for Quarter " << a.quarter[x]
+    //            << " of " << a.name[i] << " Division:$"
+    //            << a.sales[y] << endl;
+    //        y++;
+    //    }
+    //}
     file.close();
 
     return 0;
