c3f721357d9a730351c2c890fb6d27fb93198ad7
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-18 16:07:22 -0500

    Update 13-14 Corporate Sales Data Input.cpp

--- ch13 Advanced File and I-O Operations/13-14 Corporate Sales Data Input.cpp
+++ ch13 Advanced File and I-O Operations/13-14 Corporate Sales Data Input.cpp
@@ -13,9 +13,32 @@ The highest and lowest quarters for the corporation*/
 
 #include <iostream>
 #include <fstream>
+using namespace std;
+
+const int DIVISION_LENGTH = 4;
+
+struct Company {
+    static string name[DIVISION_LENGTH];
+    static int quarter[DIVISION_LENGTH];
+    double sales;
+};
+
+string Company::name[DIVISION_LENGTH] = { "East", "West", "North", "South" };
+int Company::quarter[DIVISION_LENGTH] = { 1,2,3,4 };
 
 int main()
 {
+    Company a;
+    fstream file("13-12.dat", ios::in | ios::binary);
+    double totalCorpSalesForEachQ, totalAnnualSalesForEachDiv,
+        totalAnnualCorpSales, averageQSalesForDiv, highestQForCorp, lowestQForCorp;
+
+    if (!file)
+    {
+        cout << "Error opening file." << endl;
+        return 0;
+    }
     
+
     return 0;
 }
