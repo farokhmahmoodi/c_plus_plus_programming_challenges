fcc18b509fa8c0eeda09eeb7fc881e3aa1418c66
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-20 08:38:14 -0500

    Create 13-15 Inventory Program.cpp

---
+++ ch13 Advanced File and I-O Operations/13-15 Inventory Program.cpp
@@ -0,0 +1,29 @@
+/*Write a program that uses a structure to store the following inventory information in a file:
+
+Item description
+
+Quantity on hand
+
+Wholesale cost
+
+Retail cost
+
+Date added to inventory
+
+The program should have a menu that allows the user to perform the following tasks:
+
+Add new records to the file.
+
+Display any record in the file.
+
+Change any record in the file.*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
