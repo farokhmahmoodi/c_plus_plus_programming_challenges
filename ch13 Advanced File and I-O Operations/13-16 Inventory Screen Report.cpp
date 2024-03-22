396a5ae3a83de173dcdf620c1cdabf6b40daea77
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-22 09:39:21 -0500

    Create 13-16 Inventory Screen Report.cpp

---
+++ ch13 Advanced File and I-O Operations/13-16 Inventory Screen Report.cpp
@@ -0,0 +1,21 @@
+/*Write a program that reads the information in the file created by the program in Programming Challenge 15. 
+The program should calculate and display the following information:
+
+The total wholesale value of the inventory
+
+The total retail value of the inventory
+
+The total quantity of all items in the inventory*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
