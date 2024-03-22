71e3491c77343b27442c048b5a712343faac7a45
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-22 08:33:22 -0500

    Update 13-15 Inventory Program.cpp

--- ch13 Advanced File and I-O Operations/13-15 Inventory Program.cpp
+++ ch13 Advanced File and I-O Operations/13-15 Inventory Program.cpp
@@ -21,13 +21,14 @@ Change any record in the file.*/
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 struct Inventory {
     string itemDescription;
-    int quantityOnHand;
-    double wholeSaleCost,
-        retailCost;
+    int quantityOnHand = 0;
+    double wholeSaleCost = 0.0,
+        retailCost = 0.0;
     string dateAddedToInventory;
 };
 
@@ -36,6 +37,7 @@ int main()
     Inventory a;
     fstream file("13-15.dat", ios::in | ios::out | ios::binary);
     int choice;
+    long recNum;
 
     if (!file)
     {
@@ -75,13 +77,60 @@ int main()
                 cout << "Invalid input for retail cost for new record." << endl;
             }
             cin.ignore();
-            
+            cout << "Enter date new record is added to inventory:";
+            getline(cin, a.dateAddedToInventory);
+            file.write(reinterpret_cast<char*>(&a), sizeof(a));
             break;
         case 2:
-
+            while (cout << "Which record would you like to display?"
+                && !(cin >> recNum)) {
+                cin.clear(); //clear bad input flag
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                cout << "Invalid input for record to display." << endl;
+            }
+            file.seekg(recNum * sizeof(a), ios::beg);
+            file.read(reinterpret_cast<char*>(&a), sizeof(a));
+            cout << "Item Description:" << a.itemDescription << endl;
+            cout << "Quantity on Hand:" << a.quantityOnHand << endl;
+            cout << "Wholesale Cost:$" << fixed << showpoint
+                << setprecision(2) << a.wholeSaleCost << endl;
+            cout << "Retail Cost:$" << fixed << showpoint
+                << setprecision(2) << a.retailCost << endl;
+            cout << "Date added to inventory:" << a.dateAddedToInventory << endl;
             break;
         case 3:
-
+            while (cout << "Which record would you like to edit?"
+                && !(cin >> recNum)) {
+                cin.clear(); //clear bad input flag
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                cout << "Invalid input for record to edit." << endl;
+            }
+            cout << "Enter new data below\n";
+            cout << "Item description : ";
+            getline(cin, a.itemDescription);
+            while (cout << "Quantity on hand:"
+                && !(cin >> a.quantityOnHand)) {
+                cin.clear(); //clear bad input flag
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                cout << "Invalid input for quantity on hand." << endl;
+            }
+            while (cout << "Wholesale cost:"
+                && !(cin >> a.wholeSaleCost)) {
+                cin.clear(); //clear bad input flag
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                cout << "Invalid input for wholesale cost." << endl;
+            }
+            while (cout << "Retail cost:"
+                && !(cin >> a.retailCost)) {
+                cin.clear(); //clear bad input flag
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                cout << "Invalid input for retail cost." << endl;
+            }
+            cin.ignore();
+            cout << "Date added to inventory:";
+            getline(cin, a.dateAddedToInventory);
+            file.seekp(recNum * sizeof(a), ios::beg);
+            file.write(reinterpret_cast<char*>(&a), sizeof(a));
             break;
         }
     } while (choice != '4');
