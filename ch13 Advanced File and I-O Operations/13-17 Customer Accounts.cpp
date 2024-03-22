4c130aeb6d90cb7652ede184cf1a05a06a1a62c5
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-22 11:29:30 -0500

    Create 13-17 Customer Accounts.cpp

---
+++ ch13 Advanced File and I-O Operations/13-17 Customer Accounts.cpp
@@ -0,0 +1,50 @@
+/*This program should be designed and written by a team of students. Here are some suggestions:
+
+One student should design function main, which will call other program functions or class member functions. 
+The remainder of the functions will be designed by other members of the team.
+
+The requirements of the program should be analyzed so each student is given about the same workload.
+
+Write a program that uses a structure to store the following information about a customer account:
+
+Name
+
+Address
+
+City, state, and ZIP
+
+Telephone number
+
+Account balance
+
+Date of last payment
+
+
+The structure should be used to store customer account records in a file. The program should have a menu that lets the 
+user perform the following operations:
+
+Enter new records into the file
+
+Search for a particular customer’s record and display it
+
+Search for a particular customer’s record and delete it
+
+Search for a particular customer’s record and change it
+
+Display the contents of the entire file
+
+Input Validation: When the information for a new account is entered, be sure the user enters data for all the fields. 
+No negative account balances should be entered.*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
