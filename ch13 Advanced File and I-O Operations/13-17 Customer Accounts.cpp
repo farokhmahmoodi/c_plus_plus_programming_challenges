3390afc6400fbab95db404d7b20595b3e6b93b87
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-24 23:04:51 -0500

    Add files via upload

--- ch13 Advanced File and I-O Operations/13-17 Customer Accounts.cpp
+++ ch13 Advanced File and I-O Operations/13-17 Customer Accounts.cpp
@@ -61,6 +61,8 @@ bool validZip(string);
 bool validDate(string);
 void displayRecord(fstream&, Account&);
 void deleteRecord(fstream&, Account&);
+void editRecord(fstream&, Account&);
+void displayWholeFile(fstream&, Account&);
 
 int main()
 {
@@ -100,6 +102,12 @@ int main()
                 break;
             case 3:
                 deleteRecord(file, a);
+                break;
+            case 4:
+                editRecord(file, a);
+                break;
+            case 5:
+                displayWholeFile(file, a);
                 break;
             }
         }
@@ -351,5 +359,121 @@ void deleteRecord(fstream& file, Account& a)
     }
     if (!found)
         cout << "Record not found with name entered.\n";
+    file.close();
+}
+
+void editRecord(fstream& file, Account& a)
+{
+    file.close();
+    file.open("13-17.dat", ios::in | ios::out | ios::binary);
+    bool found = false, valid;
+    char input[NAME_SIZE];
+    long recNum = 0;
+
+    cout << "Enter the name of the customer whose record you would "
+        << "like to edit:";
+    cin.getline(input, NAME_SIZE);
+    while (!file.eof())
+    {
+        file.read(reinterpret_cast<char*>(&a), sizeof(a));
+        if (strcmp(a.name, input) == 0)
+        {
+            file.seekp(recNum * sizeof(a), ios::beg);
+            cout << "Enter information for record change below.\n";
+            do
+            {
+                cout << "Name:";
+                cin.getline(a.name, NAME_SIZE);
+                valid = validName(a.name);
+                if (!valid)
+                    cout << "Invalid input for name.\n";
+            } while (!valid);
+            do
+            {
+                cout << "Address:";
+                cin.getline(a.address, NAME_SIZE);
+                valid = validAddress(a.address);
+                if (!valid)
+                    cout << "Invalid input for Address.\n";
+            } while (!valid);
+            do
+            {
+                cout << "City:";
+                cin.getline(a.city, NAME_SIZE);
+                valid = validName(a.city);
+                if (!valid)
+                    cout << "Invalid input for city.\n";
+            } while (!valid);
+            do
+            {
+                cout << "State:";
+                cin.getline(a.state, NAME_SIZE);
+                valid = validName(a.state);
+                if (!valid)
+                    cout << "Invalid input for state.\n";
+            } while (!valid);
+            do
+            {
+                cout << "ZIP code(no greater than 5 digits):";
+                getline(cin, a.zip);
+                valid = validZip(a.zip);
+                if (!valid)
+                    cout << "Invalid zip code.\n";
+            } while (!valid);
+            do
+            {
+                cout << "Telephone number in the format XXX-XXX-XXXX:";
+                getline(cin, a.telephone);
+                valid = validTelephone(a.telephone);
+                if (!valid)
+                    cout << "Invalid telephone number.\n";
+            } while (!valid);
+            do
+            {
+                while (cout << "Account balance:" &&
+                    !(cin >> a.accountBalance)) {
+                    cin.clear(); //clear bad input flag
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+                    cout << "Invalid input for account balance." << endl;
+                }
+                if (a.accountBalance < 0)
+                    cout << "Invalid input for account balance.\n";
+            } while (a.accountBalance < 0);
+            cin.ignore();
+            do
+            {
+                cout << "Date of last payment in the format MM/DD/YYYY:";
+                getline(cin, a.dateOfLastPayment);
+                valid = validDate(a.dateOfLastPayment);
+                if (!valid)
+                    cout << "Invalid date.\n";
+            } while (!valid);
+            file.write(reinterpret_cast<char*>(&a), sizeof(a));
+            found = true;
+            break;
+        }
+        recNum++;
+    }
+    if (!found)
+        cout << "Record not found with name entered.\n";
+    file.close();
+}
+
+void displayWholeFile(fstream& file, Account& a)
+{
+    file.close();
+    file.open("13-17.dat", ios::in | ios::out | ios::binary);
+    while (!file.eof())
+    {
+        file.read(reinterpret_cast<char*>(&a), sizeof(a));
+        cout << "Name:" << a.name << endl;
+        cout << "Address:" << a.address << endl;
+        cout << "City, State, and ZIP:" << a.city << ", "
+             << a.state << ", " << a.zip << endl;
+        cout << "Telephone number:" << a.telephone << endl;
+        cout << "Account balance:$" << fixed << showpoint
+                << setprecision(2) << a.accountBalance << endl;
+        cout << "Date of last payment:" << a.dateOfLastPayment << endl;
+    }
     file.close();
 }
