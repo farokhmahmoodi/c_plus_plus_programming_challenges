d492436fc49ab58b276d8e2e02458ebe584dfcdb
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-25 07:58:30 -0500

    Create 13-18 Ordered by Name, Ordered by Age.cpp

---
+++ ch13 Advanced File and I-O Operations/13-18 Ordered by Name, Ordered by Age.cpp
@@ -0,0 +1,19 @@
+/*Write a program that processes a text file that contains names of people paired with ages. Each name–age pair is on a line 
+by itself, with the name coming first and separated from the age by whitespace. The program prints out the data line by line, 
+in alphabetical order of names, and then prints out the same data in ascending order of age. Here is a sample input file.
+
+Mary  45
+Anna   78
+Sophia  5
+Petros  12
+The file may have any number of lines of such data. The output should be printed to the screen as well as to an output file.*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
