13efc6951f28aa77bbdb3dd9a9e9f1fdc5449993
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-29 16:16:58 -0500

    Update 14-2 Recursive Conversion.cpp

--- ch14 Recursion/14-2 Recursive Conversion.cpp
+++ ch14 Recursion/14-2 Recursive Conversion.cpp
@@ -15,9 +15,37 @@ Demonstrate the function with a driver program.*/
 #include <iostream>
 using namespace std;
 
+void sign(int);
+
 int main()
 {
-    
+    int n;
+
+    while (cout << "Enter an integer:"
+        && !(cin >> n)) {
+        cin.clear(); //clear bad input flag
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+        cout << "Invalid input for integer." << endl;
+    }
+    sign(n);
 
     return 0;
 }
+
+void sign(int n)
+{
+    cout << "No Parking";
+    while (n > 0)
+    {
+        sign(--n);
+    }
+}
+
+//void sign(int n)
+//{
+//    while (n > 0)
+//    {
+//        cout << "No Parking";
+//        n--;
+//    }
+//}
