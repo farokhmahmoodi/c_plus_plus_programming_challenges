227172985e3e685f82ea4675808462785114ddc3
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-29 16:20:43 -0500

    Create 14-3 QuickSort Template.cpp

---
+++ ch14 Recursion/14-3 QuickSort Template.cpp
@@ -0,0 +1,13 @@
+/*Create a template version of the quickSort algorithm that will work with any data type that overloads the comparison 
+operators. Demonstrate the template with a driver function.*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+    return 0;
+}
