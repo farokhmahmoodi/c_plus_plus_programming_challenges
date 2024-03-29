c4f99169696fc12e1f4b8615f56d222255b5ed1a
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-29 14:25:36 -0500

    Create 14-2 Recursive Conversion.cpp

---
+++ ch14 Recursion/14-2 Recursive Conversion.cpp
@@ -0,0 +1,23 @@
+/*Convert the following function to one that uses recursion.
+
+
+void sign(int n)
+{
+   while (n > 0)
+   {
+      cout << "No Parking
+";
+      n−−;
+   }
+}
+Demonstrate the function with a driver program.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
