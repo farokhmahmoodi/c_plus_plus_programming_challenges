18d05361d52f3ce6be232c621985fcf4935e49af
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-03-31 14:48:38 -0500

    Add files via upload

---
+++ ch14 Recursion/14-4 Recursive Array Sum.cpp
@@ -0,0 +1,13 @@
+/*Write a function that accepts two arguments, an array of integers, and a number indicating the number of elements in the 
+array. The function should recursively calculate the sum of all the numbers in the array. Demonstrate the use of the function
+in a program that asks the user to enter an array of numbers and prints its sum.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
