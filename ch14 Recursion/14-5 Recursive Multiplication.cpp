0d2f9961b7e8eea68bd8cca010ae30a6ca470d2d
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-01 13:46:14 -0500

    Create 14-5 Recursive Multiplication.cpp

---
+++ ch14 Recursion/14-5 Recursive Multiplication.cpp
@@ -0,0 +1,13 @@
+/*Write a recursive function that accepts two arguments into the parameters x and y. The function should return the value of 
+x times y. Remember, multiplication can be performed as repeated addition:
+
+7 * 4 = 4 + 4 + 4 + 4 + 4 + 4 + 4*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
