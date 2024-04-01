6b3708e5be4de4b3701aba73733c56430618f4e1
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-01 14:29:58 -0500

    Create 14-6 Recursive Member Test.cpp

---
+++ ch14 Recursion/14-6 Recursive Member Test.cpp
@@ -0,0 +1,14 @@
+/*Write a recursive Boolean function named isMember. The function should accept three parameters: an array of integers, an 
+integer indicating the number of elements in the array, and an integer value to be searched for. The function should return 
+true if the value is found in the array or false if the value is not found. Demonstrate the use of the function in a program 
+that asks the user to enter an array of numbers and a value to be searched for.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
