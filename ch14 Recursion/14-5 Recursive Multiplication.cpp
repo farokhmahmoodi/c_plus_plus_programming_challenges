970b1e3e0f209f62d0d07b61c8d6d9e86bafca83
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-01 14:07:02 -0500

    Update 14-5 Recursive Multiplication.cpp

--- ch14 Recursion/14-5 Recursive Multiplication.cpp
+++ ch14 Recursion/14-5 Recursive Multiplication.cpp
@@ -6,8 +6,41 @@ x times y. Remember, multiplication can be performed as repeated addition:
 #include <iostream>
 using namespace std;
 
+int recursiveMultiply(int, int);
+
 int main()
 {
+    int x, y;
     
+    while (cout << "Enter an integer x to multiply:"
+        && !(cin >> x)) {
+        cin.clear(); //clear bad input flag
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+        cout << "Invalid input for integer x." << endl;
+    }
+    while (cout << "Enter an integer y to multiply:"
+        && !(cin >> y)) {
+        cin.clear(); //clear bad input flag
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+        cout << "Invalid input for integer y." << endl;
+    }
+    cout << "The product of integers x and y is " << recursiveMultiply(x, y) << endl;
+
     return 0;
+}
+
+int recursiveMultiply(int x, int y)
+{
+    int product;
+
+    if (x == 0 || y == 0)
+    {
+        return 0;
+    }
+    else if (abs(y) > 0)
+    {
+        product = x + recursiveMultiply(x, --y);
+    }
+    
+    return product;
 }
