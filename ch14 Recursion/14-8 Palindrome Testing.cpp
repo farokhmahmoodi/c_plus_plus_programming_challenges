1bc0c2140974d069f7eac4e050918ca2f0e1d832
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-02 13:30:49 -0500

    Create 14-8 Palindrome Testing.cpp

---
+++ ch14 Recursion/14-8 Palindrome Testing.cpp
@@ -0,0 +1,20 @@
+/*A palindrome is a string such as “madam”, “radar”, “dad”, and “I”, that reads the same forwards and backwards. The empty 
+string is regarded as a palindrome. Write a recursive function
+
+bool isPalindrome(string str, int lower, int upper)
+
+that returns true if and only if the part of the string str in positions lower through upper (inclusive at both ends) is a 
+palindrome. Test your function by writing a main function that repeatedly asks the user to enter strings terminated by the 
+ENTER key. These strings are then tested for palindromicity. The program terminates when the user presses the ENTER key 
+without typing any characters before it.*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   
+
+	return 0;
+}
