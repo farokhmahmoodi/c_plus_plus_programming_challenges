15980c0af60801204294eba2ab7949a18dafa046
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-02 13:23:14 -0500

    Update 14-7 String Reverser.cpp

--- ch14 Recursion/14-7 String Reverser.cpp
+++ ch14 Recursion/14-7 String Reverser.cpp
@@ -2,13 +2,27 @@
 function in a driver program.*/
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+void stringReverser(string);
 
 int main()
 {
-    
+    string n;
+
+    cout << "Enter string to be printed in reserve order:";
+    getline(cin, n);
+    stringReverser(n);
 
     return 0;
+}
+
+void stringReverser(string n)
+{
+    if (n.length() > 0)
+    {
+        cout << n[n.length() - 1];
+        stringReverser(n.substr(0, n.length() - 1));
+    }
 }
