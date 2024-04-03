46f47f81dc64fcea197310a52607c6a6648ca5ba
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-03 15:59:07 -0500

    Update 14-10 Prefix to Postfix.cpp

--- ch14 Recursion/14-10 Prefix to Postfix.cpp
+++ ch14 Recursion/14-10 Prefix to Postfix.cpp
@@ -3,11 +3,25 @@ separate line. The program should keep reading prefix expressions and converting
 entered.*/
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    string input;
+
+    cout << "Enter prefix expressions to convert to postfix.\n"
+        << "Press enter after each expression,\n"
+        << "and press enter on a blank line to quit.\n\n";
+    cout << "Enter a prefic expression to convert to postfix: ";
+    getline(cin, input);
+    while (input.size() != 0)
+    {
+
+        // Get next line of input
+        cout << "Enter a prefix expression to convert to postfix: ";
+        getline(cin, input);
+    }
 
     return 0;
 }
