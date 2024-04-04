395acc08f980a39bf28f8c7a8de2f64213a54fea
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-04 12:19:24 -0500

    Update 14-10 Prefix to Postfix.cpp

--- ch14 Recursion/14-10 Prefix to Postfix.cpp
+++ ch14 Recursion/14-10 Prefix to Postfix.cpp
@@ -4,7 +4,10 @@ entered.*/
 
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
+
+void prefixToPostFix(istringstream&);
 
 int main()
 {
@@ -17,7 +20,8 @@ int main()
     getline(cin, input);
     while (input.size() != 0)
     {
-
+        istringstream istr(input);
+        prefixToPostFix(istr);
         // Get next line of input
         cout << "Enter a prefix expression to convert to postfix: ";
         getline(cin, input);
@@ -25,3 +29,64 @@ int main()
 
     return 0;
 }
+
+void prefixToPostFix(istringstream& in)
+{
+    ostringstream post;
+    char ch = in.peek();
+    while (!isdigit(ch))
+    {
+        ch = in.get();
+        ch = in.peek();
+    }
+    while(isdigit(ch))
+    {
+        post << ch;
+        ch = in.get();
+        ch = in.peek();
+    }
+}
+
+//int prefixExpr(istream& exprStream)
+//{
+//
+//    // Peek at first non-space character in prefix expression
+//    char ch = exprStream.peek();
+//    while (isspace(ch))
+//    {
+//        ch = exprStream.get();   // Read the space character
+//        ch = exprStream.peek();  // Peek again
+//    }
+//
+//    if (isdigit(ch))
+//    {
+//        // The prefix expression is a  single number
+//        int number;
+//        exprStream >> number;
+//        return number;
+//    }
+//    else
+//    {
+//        // The prefix expression is an operator followed
+//        // by two prefix expressions: Compute values of
+//        //the prefix expressions
+//
+//        // Read the operator
+//        ch = exprStream.get();
+//
+//        // Recursively evaluate the two subexpressions
+//        int value1 = prefixExpr(exprStream);
+//        int value2 = prefixExpr(exprStream);
+//
+//        // Apply the operator
+//        switch (ch)
+//        {
+//        case '+': return value1 + value2;
+//        case '-': return value1 - value2;
+//        case '*': return value1 * value2;
+//        case '/': return value1 / value2;
+//        default:  cout << "Bad input expression";
+//            exit(1);
+//        }
+//    }
+//}
