ba93ee4c79c54e0d1e0ca9d7cfd4d9c35bbda974
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-05 15:48:45 -0500

    Update 14-10 Prefix to Postfix.cpp

--- ch14 Recursion/14-10 Prefix to Postfix.cpp
+++ ch14 Recursion/14-10 Prefix to Postfix.cpp
@@ -7,7 +7,7 @@ entered.*/
 #include <sstream>
 using namespace std;
 
-
+string prefixToPostfix(string);
 
 int main()
 {
@@ -16,16 +16,46 @@ int main()
     cout << "Enter prefix expressions to convert to postfix.\n"
         << "Press enter after each expression,\n"
         << "and press enter on a blank line to quit.\n\n";
-    cout << "Enter a prefic expression to convert to postfix: ";
+    cout << "Enter a prefix expression to convert to postfix: ";
     getline(cin, input);
     while (input.size() != 0)
     {
-        
-
+        cout << prefixToPostfix(input) << endl;
         // Get next line of input
         cout << "Enter a prefix expression to convert to postfix: ";
         getline(cin, input);
     }
 
     return 0;
+}
+
+string prefixToPostfix(string input)
+{
+    istringstream istr(input);
+    ostringstream ostr;
+
+    for (int i = 0; i < istr.str().length(); i++)
+    {
+        if (isalnum(istr.str()[i]))
+        {
+            if (isspace(istr.str()[i + 1]))
+                ostr << istr.str()[i] << " ";
+            else
+                ostr << istr.str()[i];
+        }
+    }
+    ostr << " ";
+    for (int i = 0; i < istr.str().length(); i++)
+    {
+        if (istr.str()[i] == '+' || istr.str()[i] == '-'
+            || istr.str()[i] == '*' || istr.str()[i] == '/')
+        {
+            if (isspace(istr.str()[i + 1]))
+                ostr << istr.str()[i] << " ";
+            else
+                ostr << istr.str()[i];
+        }
+    }
+    
+    return ostr.str();
 }
