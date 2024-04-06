29720ed5c6aa2510826b58a648376b8429ea2e9d
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-05 22:17:15 -0500

    Add files via upload

--- ch14 Recursion/14-11 Prefix to Infix.cpp
+++ ch14 Recursion/14-11 Prefix to Infix.cpp
@@ -3,13 +3,82 @@ parenthesized to show the order of application of the operators. Each prefix exp
 The program should keep reading prefix expressions and converting them to infix until a blank line is entered.*/
 
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
-
+string prefixToInfix(istream&);
 
 int main()
 {
-    
+    string input;
+
+    cout << "Enter prefix expressions to convert to infix.\n"
+        << "Press enter after each expression,\n"
+        << "and press enter on a blank line to quit.\n\n";
+    cout << "Enter a prefix expression to convert to infix: ";
+    getline(cin, input);
+    while (input.size() != 0)
+    {
+        istringstream istr(input);
+        cout << prefixToInfix(istr) << endl;
+        // Get next line of input
+        cout << "Enter a prefix expression to convert to infix: ";
+        getline(cin, input);
+    }
 
     return 0;
+}
+
+string prefixToInfix(istream& exprStream)
+{
+    // Peek at first non-space character in prefix expression
+    char ch = exprStream.peek();
+
+    while (isspace(ch))
+    {
+        ch = exprStream.get();   // Read the space character
+        ch = exprStream.peek();  // Peek again
+    }
+    if (isalpha(ch))
+    {
+        // The prefix expression is a single variable
+        string var;
+        exprStream >> var;
+        return var;
+    }
+    if (isdigit(ch))
+    {
+        // The prefix expression is a single number
+        int num;
+        exprStream >> num;
+        ostringstream ostr;
+        ostr << num;
+        return ostr.str();
+    }
+    else
+    {
+        // The prefix expression is an operator followed
+        // by two prefix expressions:
+
+        // Read the operator
+        ch = exprStream.get();
+        cout << "ch:" << ch << endl;
+
+        // Recursively evaluate the two subexpressions
+        string value1 = prefixToInfix(exprStream);
+        cout << "value1:" << value1 << endl;
+        string value2 = prefixToInfix(exprStream);
+        cout << "value2:" << value2 << endl;
+
+        // Apply the operator
+        switch (ch)
+        {
+        case '+': return "(" + value1 + " + " + value2 + ")";
+        case '-': return "(" + value1 + " - " + value2 +")";
+        case '*': return "(" + value1 + " * " + value2 + ")";
+        case '/': return "(" + value1 + " / " + value2 + ")";
+        default:  return "Bad input expression";
+        }
+    }
 }
