ce93c17bc26f56bf552cb484b5ea9b7ad1089e27
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-08 13:10:27 -0500

    Create 15-1 Analysis of Sorting Algorithms.cpp

---
+++ ch15 Polymorphism and Virtual Functions/15-1 Analysis of Sorting Algorithms.cpp
@@ -0,0 +1,18 @@
+/*Design a class AbstractSort that can be used to analyze the number of comparisons performed by a sorting algorithm. The 
+class should have a member function compare that is capable of comparing two array elements, and a means of keeping track of 
+the number of comparisons performed. The class should be an abstract class with a pure virtual member function
+
+void sort(int arr[ ], int size)
+which, when overridden, will sort the array by calling the compare function to determine the relative order of pairs of 
+numbers. Create a subclass of AbstractSort that uses a simple sorting algorithm to implement the sort function. The class 
+should have a member function that can be called after the sorting is done to retrieve the number of comparisons performed.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
