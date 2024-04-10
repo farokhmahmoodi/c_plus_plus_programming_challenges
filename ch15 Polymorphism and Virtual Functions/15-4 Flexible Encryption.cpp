7f07c9d2e0a744f1e3a36df091992eb18bbfa5ce
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-10 10:27:07 -0500

    Update 15-4 Flexible Encryption.cpp

--- ch15 Polymorphism and Virtual Functions/15-4 Flexible Encryption.cpp
+++ ch15 Polymorphism and Virtual Functions/15-4 Flexible Encryption.cpp
@@ -1,1 +1,16 @@
+/*Write a modification of the encryption program of Section 15.5 whose transform function uses an integer key to transform 
+the character passed to it. The function transforms the character by adding the key to it. The key should be represented as a 
+member of the Encryption class, and the class should be modified so that it has a member function that sets the encryption 
+key. When the program runs, the main function should ask the user for the input file, the output file, and an encryption key.
 
+Show that with these modifications, the same program can be used for both encryption and decryption.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
