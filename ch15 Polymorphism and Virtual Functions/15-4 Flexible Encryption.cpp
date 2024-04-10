add9832e2c5426b2de0371eeb2ae432307495f01
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-10 16:34:49 -0500

    Update 15-4 Flexible Encryption.cpp

--- ch15 Polymorphism and Virtual Functions/15-4 Flexible Encryption.cpp
+++ ch15 Polymorphism and Virtual Functions/15-4 Flexible Encryption.cpp
@@ -6,11 +6,107 @@ key. When the program runs, the main function should ask the user for the input 
 Show that with these modifications, the same program can be used for both encryption and decryption.*/
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
 using namespace std;
+
+class Encryption
+{
+protected:
+    ifstream inFile;
+    ofstream outFile;
+    int key;
+public:
+    Encryption(const string& inFileName, const string& outFileName,int k);
+    virtual  ~Encryption();
+    // Pure virtual function
+    virtual char transform(char ch, int key) const = 0;
+    // Do the actual work.
+    virtual void encrypt() final;
+};
+
+//**************************************************
+// Constructor opens the input and output file.    *
+//**************************************************
+Encryption::Encryption(const string& inFileName, const string& outFileName, int k)
+{
+    inFile.open(inFileName);
+    outFile.open(outFileName);
+    if (!inFile)
+    {
+        cout << "The file " << inFileName
+            << " cannot be opened.";
+        exit(1);
+    }
+    if (!outFile)
+    {
+        cout << "The file " << outFileName
+            << " cannot be opened.";
+        exit(1);
+    }
+    key = k;
+}
+
+//**************************************************
+//Destructor closes files.                         *
+//**************************************************
+Encryption::~Encryption()
+{
+    inFile.close();
+    outFile.close();
+}
+
+//*****************************************************
+//Encrypt function uses the virtual transform         *
+//member function to transform individual characters. *
+//***************************************************** 
+void Encryption::encrypt()
+{
+    char ch;
+    char transCh;
+    inFile.get(ch);
+    while (!inFile.fail())
+    {
+        transCh = transform(ch,key);
+        outFile.put(transCh);
+        inFile.get(ch);
+    }
+}
+
+// The subclass simply overides the virtual
+// transformation function
+class SimpleEncryption : public Encryption
+{
+public:
+    char transform(char ch, int key) const override
+    {
+        return ch + key;
+    }
+    SimpleEncryption(const string& inFileName, const string& outFileName,int k)
+        : Encryption(inFileName, outFileName,k)
+    {
+    }
+};
 
 int main()
 {
-    
+    string inFileName, outFileName;
+    int k;
+
+    cout << "Enter name of file to encrypt: ";
+    cin >> inFileName;
+    cout << "Enter name of file to receive "
+        << "the encrypted text: ";
+    cin >> outFileName;
+    while (cout << "Enter an integer for encryption key:" &&
+        !(cin >> k)) {
+        cin.clear(); //clear bad input flag
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discard input
+        cout << "Invalid input for integer." << endl;
+    }
+    SimpleEncryption obfuscate(inFileName, outFileName,k);
+    obfuscate.encrypt();
 
     return 0;
 }
