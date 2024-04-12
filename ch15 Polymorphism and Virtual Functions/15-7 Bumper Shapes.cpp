b6188b43085a4bafa8d3aa8cf649f133fe943d32
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-12 10:52:24 -0500

    Update 15-7 Bumper Shapes.cpp

--- ch15 Polymorphism and Virtual Functions/15-7 Bumper Shapes.cpp
+++ ch15 Polymorphism and Virtual Functions/15-7 Bumper Shapes.cpp
@@ -7,25 +7,22 @@ The program terminates when the shapes meet each other in the middle for the ten
 
 int main()
 {
-    // Create a tent and a box
-    shared_ptr<Tent> tent = make_shared<Tent>(20, 10, 13);
+    // Create a box
     shared_ptr<Box> box = make_shared<Box>(5, 10, 4, 7);
 
-    // Draw the tent and the box
-    tent->draw();
+    // Draw the box
     box->draw();
     // Set direction of motion for the two shapes
-    tent->setDirection(-1, 0);  // Tent moves straight up
     box->setDirection(0, 1);    // Box moves horizontally to the right
 
     // Simultaneously move the tent and the box
     for (int k = 0; k < 12; k++)
     {
         Sleep(75);
-        tent->move();
+
         box->move();
     }
-    box->move(); tent->move();
+    box->move();
 
     // Move the box farther to the right
     for (int k = 0; k < 48; k++)
@@ -33,12 +30,11 @@ int main()
         Sleep(75);
         box->move();
     }
-    // Create a complex shape composed of the tent and the box
-    vector<shared_ptr<Shape>> myShapes{ tent, box };
+    // Create a complex shape composed of the box
+    vector<shared_ptr<Shape>> myShapes{ box, box };
     shared_ptr<ComplexShape> cS = make_shared<ComplexShape>(myShapes);
 
     // Set directions for the two shapes
-    tent->setDirection(1, 0);
     box->setDirection(0, -1);
     // Move the complex shape: this moves both the tent and the box
     for (int k = 0; k < 12; k++)
