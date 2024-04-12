19f509d379a27e2614d69f5fa17f63cd5215e763
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-12 11:58:40 -0500

    Update ShapeAnimator2.cpp

--- ch15 Polymorphism and Virtual Functions/ShapeAnimator2.cpp
+++ ch15 Polymorphism and Virtual Functions/ShapeAnimator2.cpp
@@ -1,82 +1,132 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <vector>
-#include <memory>
-#include <windows.h>
-using namespace std;
+#include "ShapeAnimator2.h"
 
-const HANDLE outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-
-// A shape has a direction and is able to move in that direction.
-// The move is a virtual member function.
-class Shape
+//*************************************************************
+// Moves a simple shape one step by erasing the shape         *
+// at its current position, changing its position, and then   *
+// redrawing the shape at its new position.                   *
+//*************************************************************
+void SimpleShape::move()
 {
-public:
-    void setDirection(int drow, int dcol)
-    {
-        dRow = drow; dCol = dcol;
-    }
-    void getDirection(int& drow, int& dcol) const
-    {
-        drow = dRow; dcol = dCol;
-    }
-    virtual void move() = 0;
-private:
-    int dCol, dRow; // Direction of motion
-};
+    int dRow, dCol; // Direction of motion
+    int savedColor = color;
+    color = 0;      // Drawing in color 0 erases the shape
+    draw();
+    // Compute the new postion for the shape by adding a step in 
+    // the proper direction to the current position
+    getDirection(dRow, dCol);
+    rowPos += dRow;
+    colPos += dCol;
+    // Draw the shape at its new position in its specified color
+    color = savedColor;
+    draw();
+}
 
-// A SimpleShape can be drawn at a given position in a specified color
-class SimpleShape : public Shape
+//***********************************
+// Draws a tent at its position     *
+//***********************************
+void Tent::draw() const
 {
-public:
-    virtual void draw() const = 0;
-    void getPosition(int& row, int& col) const
-    {
-        row = rowPos; col = colPos;
-    }
-    void setPosition(int row, int col)
-    {
-        rowPos = row; colPos = col;
-    }
-    void setColor(int c) { color = c; }
-    int getColor() const { return color; }
-    virtual void move() override;
-private:
-    int color;
     int rowPos, colPos;
-};
+    COORD pos;
+    int currentLength = length;
+    // Set the color attribute
+    SetConsoleTextAttribute(outHandle, getColor());
+    getPosition(rowPos, colPos);
+    pos.Y = rowPos; pos.X = colPos;
 
-// A Box is a rectangular type of shape
-class Box : public SimpleShape
+    // Draw the lines that form the tent beginning with
+    // the base and moving up toward the point
+    for (int r = 0; r < (length + 1) / 2; r++)
+    {
+        SetConsoleCursorPosition(outHandle, pos);
+        for (int k = 0; k < currentLength; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        pos.Y--;
+        pos.X++;
+        currentLength -= 2;
+    }
+    // Restore normal attribute
+    SetConsoleTextAttribute(outHandle, 7);
+}
+
+//**********************************
+// Draws a box shape               *
+//**********************************
+void Box::draw() const
 {
-public:
-    virtual void draw() const override;
-    Box(int rowPos, int colPos, int width, int height);
-private:
-    int width, height;
-};
+    int rowPos, colPos;
+    COORD pos;
 
-// A Tent is an isosceles triangle whose horizontal base has a 
-// given length and whose height is half the length of the base.
-// The position of the triangle is the left endpoint of the base
-class Tent : public SimpleShape
+    // Set the color attribute for the box
+    SetConsoleTextAttribute(outHandle, getColor());
+    getPosition(rowPos, colPos);
+    pos.X = colPos; pos.Y = rowPos;
+
+    // Draw the lines that make up the  box
+    for (int r = 0; r < height; r++)
+    {
+        SetConsoleCursorPosition(outHandle, pos);
+        for (int c = 0; c < width; c++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        pos.Y++;
+    }
+    // Restore normal text attribute
+    SetConsoleTextAttribute(outHandle, 7);
+}
+
+//***********************************************
+// Constructor sets the color, position, and    *
+// dimensions for a box shape, and draws        *
+// the box at its initial position              *
+//***********************************************
+Box::Box(int rowPos, int colPos, int width, int height)
 {
-public:
-    virtual void draw() const override;
-    Tent(int baseRowPos, int baseColPos, int length);
-private:
-    int length;
-};
-
-// A ComplexShape is made up of simpler shapes. It is represented
-// as a vector of pointers to the simpler shapes that make it up
-class ComplexShape : public Shape
+    setColor(4);
+    setPosition(rowPos, colPos);
+    this->width = width;
+    this->height = height;
+    draw();
+}
+//***********************************************
+// Constructor sets the color for a Tent shape, *
+// sets the position of the tent as well as the *
+// length of its base and draws it at its       *
+// initial position                             *
+//***********************************************
+Tent::Tent(int baseRowPos, int baseColPos, int length)
 {
-public:
-    ComplexShape(const vector<shared_ptr<Shape>>& shapeCollection);
-    virtual void move() override;
-private:
-    vector<shared_ptr<Shape>>shapes;
-};
+    setColor(2);
+    setPosition(baseRowPos, baseColPos);
+    this->length = length;
+    draw();
+}
 
+//*******************************************************
+// Constructor builds complex shape by assembling a     *
+// vector of constituent shapes                         *
+//*******************************************************
+ComplexShape::
+ComplexShape(const vector<shared_ptr<Shape>>& shapeCollection)
+{
+    for (int k = 0; k < shapeCollection.size(); k++)
+    {
+        auto p = shapeCollection[k];
+        shapes.push_back(p);
+    }
+}
+
+//**************************************
+// Moves a complex shape by moving the *
+// constituent shapes                  *
+//**************************************
+void ComplexShape::move()
+{
+    for (int k = 0; k < shapes.size(); k++)
+        shapes[k]->move();
+}
