6966051987186491e2618ecc6556c7b90ee17303
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-12 08:18:52 -0500

    Create 15-7 Bumper Shapes.cpp

---
+++ ch15 Polymorphism and Virtual Functions/15-7 Bumper Shapes.cpp
@@ -0,0 +1,14 @@
+/*Write a program that creates two rectangular shapes and then animates them. The two shapes should start on opposite ends of 
+the screen and then move toward each other. When they meet in the middle of the screen, each shape reverses course and moves 
+toward the edge of the screen. The two shapes keep oscillating and bouncing off of each other in the middle of the screen. 
+The program terminates when the shapes meet each other in the middle for the tenth time.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
