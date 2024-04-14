359f7f38cc8a4fe6f61a9f76d2b92398d68439fc
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-14 13:29:00 -0500

    Add files via upload

---
+++ ch16 Exceptions and Templates/16-1 String Bound Exceptions.cpp
@@ -0,0 +1,26 @@
+/*Write a class BCheckString that is derived from the STL string class. This new class will have two member 
+functions:
+
+A.)CheckString(string s) constructor that receives a string object passed by value and passes it on to the base 
+class constructor.
+B.)An char operator[](int k) function that throws a BoundsException object if k is negative or is greater 
+than or equal to the length of the string. If k is within the bounds of the string, this function will 
+return the character at position k in the string.
+
+You will need to write the definition of the BoundsException class. Test your class with a main function that 
+attempts to access characters that are within and outside the bounds of a suitably initialized BCheckString 
+object.*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main()
+{
+	
+
+	return 0;
+}
