9bfc008380409dcee6a228d3ba315b33455acdd9
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-14 21:36:56 -0500

    Add files via upload

---
+++ ch16 Exceptions and Templates/16-4 Sequence Accumulation.cpp
@@ -0,0 +1,26 @@
+/*Write a function
+
+T accum(vector <T>  v)
+
+that forms and returns the “sum” of all items in the vector v passed to it. For example, if T is a numeric type 
+such as int or double, the numeric sum will be returned, and if T represents the STL string type, then the 
+result of concatenation is returned.
+
+Note
+For any type T, the expression T() yields the value or object created by the default constructor. For example, 
+T() yields the empty string object if T is the string class. If T represents a numeric type such as int, then 
+T() yields 0. Use this fact to initialize your “accumulator.”
+
+Test your function with a driver program that asks the user to enter three integers, uses accum to 
+compute the sum, and prints out the sum. The program than asks the user to enter three strings, uses 
+accum to concatenate the strings, and prints the result.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
