95f641efdf2e5d942ebd573a47bd20d2639f2854
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-16 16:20:28 -0500

    Create 16-8 Searchable Modification.cpp

---
+++ ch16 Exceptions and Templates/16-8 Searchable Modification.cpp
@@ -0,0 +1,12 @@
+/*Modify the SearchableVector class template, presented in this chapter, so it performs a binary search instead 
+of a linear search. Test the template in a driver program.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
