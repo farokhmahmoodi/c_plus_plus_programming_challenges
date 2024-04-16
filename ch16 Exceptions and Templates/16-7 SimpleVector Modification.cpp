85127e26874be127281880dfb92c03644af5df9a
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-16 12:24:22 -0500

    Create 16-7 SimpleVector Modification.cpp

---
+++ ch16 Exceptions and Templates/16-7 SimpleVector Modification.cpp
@@ -0,0 +1,15 @@
+/*Modify the SimpleVector class template, presented in this chapter, to include the member functions push_back and pop_back. 
+These functions should emulate the STL vector class member functions of the same name. The push_back function should throw 
+an exception if the array is full. The push_back function should accept an argument and insert its value at the end of the 
+array. The pop_back function should accept no argument and remove the last element from the array. Test the class with a 
+driver program.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
