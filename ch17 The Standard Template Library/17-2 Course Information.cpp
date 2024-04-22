abdaa48342d143db761a39d044c7240eb35ee16a
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-21 19:48:12 -0500

    Add files via upload

---
+++ ch17 The Standard Template Library/17-2 Course Information.cpp
@@ -0,0 +1,42 @@
+/*Write a program that creates a map containing course numbers and the room numbers of the rooms where the 
+courses meet. The map should have the following key-value pairs:
+
+Course Number (key)	Room Number (value)
+CS101	3004
+CS102	4501
+CS103	6755
+NT110	1244
+CM241	1411
+
+The program should also create a map containing course numbers and the names of the instructors that teach 
+each course. The map should have the following key-value pairs:
+
+Course Number (key)	Instructor (value)
+CS101	Haynes
+CS102	Alvarado
+CS103	Rich
+NT110	Burke
+CM241	Lee
+
+The program should also create a map containing course numbers and the meeting times of each course. The map 
+should have the following key-value pairs:
+
+Course Number (key)	Meeting Time (value)
+CS101	8:00 a.m.
+CS102	9:00 a.m.
+CS103	10:00 a.m.
+NT110	11:00 a.m.
+CM241	1:00 p.m.
+
+The program should let the user enter a course number, and then it should display the course’s room number, 
+instructor, and meeting time.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
