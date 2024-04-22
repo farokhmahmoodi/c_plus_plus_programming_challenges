2e4e12e2647a0e5d0ff0e706daf5dd6e0e93e090
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-21 20:27:06 -0500

    Add files via upload

---
+++ ch17 The Standard Template Library/17-3 Encryption Decryption Key.cpp
@@ -0,0 +1,26 @@
+/*Write a program that creates an encryption/decryption key and stores it in a text file. Messages to be 
+encrypted/decrypted will use only lower case letters, the blank space character, and the new line character. The 
+encryption key, which will also double as the decryption key, will be a pair of sequences of characters 'a', .. ,
+'z', the blank character, and new line character. The first sequence will list all the characters in some order, 
+and the second will be a random permutation of the first. The program should store both the original and the 
+scrambled sequence in a file called “keyfile.txt” and close the file. The program should then open the file for 
+reading and echo its contents to the screen. Here is a possible output of the program.
+
+abcdefghijklmnopqrstuvwxyz
+
+ebatq
+jy spcxdhrkfiwuzlvngmo
+
+In this case, 'a' is mapped to 'e','b' to itself, 'c' to 'a', 'e' to 'q', 'f' to the new line character, 
+'g' to 'j','i' to the blank space, 'z' to 'g', the blank space to 'm', and the new line character to the 
+character 'o'.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
