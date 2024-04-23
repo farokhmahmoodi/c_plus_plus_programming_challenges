fe87af38b687e85542d82abb7f25dc3f80e2ebdb
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-23 10:59:19 -0500

    Create 17-4 File Encryption.cpp

---
+++ ch17 The Standard Template Library/17-4 File Encryption.cpp
@@ -0,0 +1,14 @@
+/*Write an encryption program that reads two text files: the “keyfile.txt” created in the preceding problem, 
+and a second file “plain.txt” containing a message to be encrypted using the key in the “keyfile.txt” file. The 
+message will consist only of lowercase characters, spaces and new lines. The message is encrypted by replacing 
+each character in the original sequence with the character at the same position in the scrambled sequence. The 
+output should be echoed to the screen and simultaneously written to a file “cipher.txt.”*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
