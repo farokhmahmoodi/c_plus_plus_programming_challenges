180170747211119e8d6893fe0e343bd68c808b22
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-24 03:03:22 -0500

    Add files via upload

---
+++ ch17 The Standard Template Library/17-6 Word Frequency.cpp
@@ -0,0 +1,19 @@
+/*Write a program that reads the contents of a text file. The program should create a map in which the keys
+are the individual words found in the file and the values are the number of times each word appears.
+For example, if the word “the” appears 128 times, the map would contain an element with "the" as the key
+and 128 as the value. The program should display the frequency of each word on the screen and create a
+second file containing a list of each word and its frequency. Consider a word to be any run of characters
+delimited by white space.*/
+
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
