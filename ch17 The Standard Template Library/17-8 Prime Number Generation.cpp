8293504cf603c29ffb7a6cde1e46a9348adc9610
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-24 17:34:31 -0500

    Add files via upload

---
+++ ch17 The Standard Template Library/17-8 Prime Number Generation.cpp
@@ -0,0 +1,20 @@
+/*A positive integer greater than 1 is said to be prime if it has no divisors other than 1 and itself. 
+A positive integer greater than 1 is composite if it is not prime. Write a program that asks the user to 
+enter an integer greater than 1, and then displays all of the prime numbers that are less than or equal to 
+the number entered. The program should work as follows:
+
+Once the user has entered a number, the program should populate a vector with all of the integers from 2, up 
+through the value entered.
+
+The program should then use the STL's for_each function to step through the vector. The for_each function 
+should pass each element to a function object that displays the element if it is a prime number.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
