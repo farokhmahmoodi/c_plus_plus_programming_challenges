94b19a41ba518c0154843000de06dc83ebee4ed5
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-04-26 00:34:40 -0500

    Update 17-9 Gas Prices.cpp

--- ch17 The Standard Template Library/17-9 Gas Prices.cpp
+++ ch17 The Standard Template Library/17-9 Gas Prices.cpp
@@ -1,35 +1,35 @@
-/*In the student sample program files for this chapter, you will find a text file named GasPrices.txt. 
+/*In the student sample program files for this chapter, you will find a text file named GasPrices.txt.
 The file contains the weekly average prices for a gallon of gas in the United States, beginning on April 5, 1993,
 and ending on August 26, 2013. Figure 17-10 shows an example of the first few lines of the file's contents:
 
 Figure 17-10 Contents of the GasPrices.txt File
 
-Each line in the file contains the average price for a gallon of gas on a specific date. Each line is formatted 
+Each line in the file contains the average price for a gallon of gas on a specific date. Each line is formatted
 in the following way:
 
 MM-DD-YYYY:Price
-MM is the two-digit month, DD is the two-digit day, and YYYY is the four-digit year. Price is the average price 
+MM is the two-digit month, DD is the two-digit day, and YYYY is the four-digit year. Price is the average price
 per gallon of gas on the specified date.
 
-For this assignment you are to write one or more programs that read the contents of the file and perform the 
+For this assignment you are to write one or more programs that read the contents of the file and perform the
 following calculations:
 
-AVERAGE PRICE PER YEAR: Calculate the average price of gas per year, for each year in the file. (The file's 
+AVERAGE PRICE PER YEAR: Calculate the average price of gas per year, for each year in the file. (The file's
 data starts in April 1993, and it ends in August 2013. Use the data that is present for the years 1993 and 2013.)
 
 AVERAGE PRICE PER MONTH: Calculate the average price for each month in the file.
 
-HIGHEST AND LOWEST PRICES PER YEAR: For each year in the file, determine the date and amount for the lowest 
+HIGHEST AND LOWEST PRICES PER YEAR: For each year in the file, determine the date and amount for the lowest
 price, and the highest price.
 
-LIST OF PRICES, LOWEST TO HIGHEST: Generate a text file that lists the dates and prices, sorted from the 
+LIST OF PRICES, LOWEST TO HIGHEST: Generate a text file that lists the dates and prices, sorted from the
 lowest price to the highest.
 
-LIST OF PRICES, HIGHEST TO LOWEST: Generate a text file that lists the dates and prices, sorted from the 
+LIST OF PRICES, HIGHEST TO LOWEST: Generate a text file that lists the dates and prices, sorted from the
 highest price to the lowest.
 
-You can write one program to perform all of these calculations, or you can write different programs, one 
-for each calculation. Regardless of the approach that you take, you should read the contents of the 
+You can write one program to perform all of these calculations, or you can write different programs, one
+for each calculation. Regardless of the approach that you take, you should read the contents of the
 GasPrices.txt file, and extract its data into one or more STL containers appropriate for your algorithm.*/
 
 #include <iostream>
@@ -44,10 +44,9 @@ using namespace std;
 int main()
 {
     ifstream in("GasPrices.txt", ios::in);
-    int month,day,year;
+    int month, day, year;
     double price;
     string line;
-    char dash1, dash2, colon;
     map<int, vector<double>> avgPricePerYear;
 
     if (!in)
@@ -57,9 +56,8 @@ int main()
     }
     while (getline(in, line))
     {
-        getline(in, line);
-        istringstream istr(line);
-        istr >> month >> dash1 >> day >> dash2 >> year >> colon >> price;
+        
+        
     }
     in.close();
 
