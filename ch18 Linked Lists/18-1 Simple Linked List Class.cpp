8aa286362f165db580913e156ee2436b875d5d99
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-02 22:46:33 -0500

    Add files via upload

---
+++ ch18 Linked Lists/18-1 Simple Linked List Class.cpp
@@ -0,0 +1,20 @@
+/*Using an appropriate definition of ListNode, design a simple linked list class with only two 
+member functions and a default constructor:
+
+
+void add(double x);
+boolean isMember(double x);
+LinkedList( );
+
+The add function adds a new node containing x to the front (head) of the list, while the isMember 
+function tests to see if the list contains a node with the value x. Test your linked list class 
+by adding various numbers to the list and then testing for membership.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
