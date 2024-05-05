447e6709cf78501635483ece64a7a9ef0181106f
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-05 14:23:52 -0500

    Add files via upload

---
+++ ch18 Linked Lists/18-7 List Search.cpp
@@ -0,0 +1,148 @@
+﻿/*Modify the list class of Programming Challenge 1 (or later) to include a member function
+
+int search(double x)
+
+that returns the position of a number x on the list. The first node in the list is at position 0, 
+the second node is at position 1, and so on. If x is not found on the list, the search should 
+return −1. Test the new member function using an appropriate driver program.*/
+
+#include <iostream>
+using namespace std;
+
+class LinkedList
+{
+protected:
+    // Declare a class for the list node
+    struct ListNode
+    {
+        double value;
+        ListNode* next;
+        ListNode(double value1, ListNode* next1 = nullptr)
+        {
+            value = value1;
+            next = next1;
+        }
+    };
+    ListNode* head;
+public:
+    void add(double x)
+    {
+        if (head == nullptr) //if list is empty
+        {
+            head = new ListNode(x);
+        }
+        else //if list is not empty
+        {
+            head = new ListNode(x, head);
+        }
+    }
+    bool isMember(double x)
+    {
+        return isMember(head, x);
+    }
+    LinkedList()
+    {
+        head = nullptr;
+    }
+    LinkedList(const LinkedList& original)
+    {
+        head = copyList(original.head);
+    }
+    void print() const
+    {
+        ListNode* nodePtr = head;
+        while (nodePtr)
+        {
+            cout << nodePtr->value << " ";
+            nodePtr = nodePtr->next;
+        }
+        cout << endl;
+    }
+    void remove(double x)
+    {
+        head = remove(head, x);
+    }
+    void reverse()
+    {
+        ListNode* current = head, * previous = nullptr, * next = nullptr;
+
+        if (head != NULL)
+        {
+            while (current->next != nullptr)
+            {
+                next = current->next;
+                current->next = previous;
+                previous = current;
+                current = next;
+            }
+            current->next = previous;
+            head = current;
+        }
+    }
+    ~LinkedList()
+    {
+        ListNode* ptr = head;
+        while (ptr != nullptr)
+        {
+            ListNode* garbage = ptr;
+            ptr = ptr->next;
+            delete garbage;
+        }
+    }
+private:
+    static ListNode* copyList(ListNode* aList);
+    ListNode* remove(ListNode* aList, double x)
+    {
+        if (aList == nullptr)
+            return nullptr;
+        if (aList->value == x)
+        {
+            ListNode* tail = aList->next;
+            delete aList;
+            return tail;
+        }
+        else
+        {
+            aList->next = remove(aList->next, x);
+            return aList;
+        }
+    }
+    bool isMember(ListNode* aList, double x)
+    {
+        if (aList == nullptr) //if list is empty
+            return false;
+        else if (aList->value == x)
+            return true;
+        else
+        {
+            return isMember(aList->next, x);
+        }
+    }
+};
+
+LinkedList::ListNode* LinkedList::copyList(ListNode* aList)
+{
+    if (aList == nullptr)
+        return nullptr;
+    else
+    {
+        // First copy the tail
+        ListNode* tailCopy = copyList(aList->next);
+        // Return copy of head attached to copy of tail
+        return new ListNode(aList->value, tailCopy);
+    }
+}
+
+int main()
+{
+    LinkedList a;
+
+    a.add(745.234);
+    a.add(98.56);
+    a.add(-12.34);
+    a.add(14.24);
+    a.add(245.984);
+    a.print();
+
+    return 0;
+}
