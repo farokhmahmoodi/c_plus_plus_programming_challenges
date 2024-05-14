52389d74b5df350584d5e8fb9ede86b0f02333a3
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-14 10:38:37 -0500

    Update 18-11 Generation of Subsets.cpp

--- ch18 Linked Lists/18-11 Generation of Subsets.cpp
+++ ch18 Linked Lists/18-11 Generation of Subsets.cpp
@@ -16,8 +16,11 @@ int main()
     list<vector<int>> LkMinus1;
     vector<int> v = {1,2,3}, subset;
 
-    for(auto it = v.begin(); it != v.end(); it++) //creating list Lk-1 of all subsets of 1,2,..,k-1
+    //creating list Lk-1 of all subsets of 1,2,..,k-1
+    for(auto it = v.begin(); it != v.end(); it++)
     {
+        if(subset.size() == 0 && it == v.begin())
+            LkMinus1.emplace_back(subset);
         subset.emplace_back(*it);
         LkMinus1.emplace_back(subset);
         for(auto it2 = it + 1; it2 != v.end(); it2++)
@@ -32,20 +35,22 @@ int main()
         LkMinus1.emplace_back(v);
     subset.shrink_to_fit();
     list<vector<int>> L;
-    for(auto it = LkMinus1.begin(); it != LkMinus1.end(); it++) //creating list L by adding integer k to each vector in
-    {                                                           //list Lk-1
+    //creating list L by adding integer k to each vector in list Lk-1
+    for(auto it = LkMinus1.begin(); it != LkMinus1.end(); it++)
+    {
         subset = *it;
-        L.emplace_back(subset);
+        if(subset.size() != 0)
+            L.emplace_back(subset);
         subset.emplace_back(4);
         L.emplace_back(subset);
         subset.clear();
     }
-    subset.emplace_back(4);
-    L.emplace_back(subset);
     subset.clear();
     subset.shrink_to_fit();
-    list<vector<int>> Lk(L); //creating list Lk by setting it to list L
-    for (auto elem : Lk) //printing list of all subsets
+    /*creating list Lk by setting it to list L
+    printing list of all subsets*/
+    list<vector<int>> Lk(L);
+    for (auto elem : Lk)
     {
         for(int i = 0; i < elem.size();i++)
             cout << elem[i] << " ";
