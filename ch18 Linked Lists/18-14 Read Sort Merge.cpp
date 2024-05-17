ca6ecec3bf237cb6feb2834636d38a5396f7ae96
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-17 12:25:08 -0500

    Update 18-14 Read Sort Merge.cpp

--- ch18 Linked Lists/18-14 Read Sort Merge.cpp
+++ ch18 Linked Lists/18-14 Read Sort Merge.cpp
@@ -12,11 +12,7 @@ The merge function should not make copies of nodes: Rather, it must remove nodes
 
 Test your functions by having the user enter two sequences of numbers, sorting each sequence, and then merging and printing the resulting list.*/
 
-
-// This program illustrates the building
-// and traversal of a linked list.
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 struct ListNode
@@ -33,31 +29,6 @@ struct ListNode
 
 int main()
 {
-   double number;                    // Used to read the file
-   ListNode *numberList = nullptr;   // List of numbers
 
-   // Open the file
-   ifstream numberFile("numberFile.dat");
-   if (!numberFile)
-   {
-      cout << "Error in opening the file of numbers.";
-      exit(1);
-   }
-   // Read the file into a linked list
-   cout << "The contents of the file are: " << endl;
-   while (numberFile >> number)
-   {
-      cout << number << "  ";
-      // Create a node to hold this number
-      numberList = new ListNode(number, numberList);
-   }
-   // Traverse the list while printing
-   cout << endl << "The contents of the list are: " << endl;
-   ListNode *ptr = numberList;
-   while (ptr != nullptr)
-   {
-      cout << ptr->value << "  "; // Process node
-      ptr = ptr->next;            // Move to next node
-   }
    return 0;
 }
