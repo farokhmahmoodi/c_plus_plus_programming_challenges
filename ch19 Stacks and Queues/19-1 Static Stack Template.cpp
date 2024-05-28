c70707706343af7123df885dd7663bbbe11d5698
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-27 22:12:04 -0500

    Add files via upload

--- ch19 Stacks and Queues/19-1 Static Stack Template.cpp
+++ ch19 Stacks and Queues/19-1 Static Stack Template.cpp
@@ -65,6 +65,23 @@ void IntStack::pop(int& value)
 
 int main()
 {
-    
+    IntStack  stack(5);
+    int values[] = { 5, 10, 15, 20, 25 };
+    int value;
+
+    cout << "Pushing...\n";
+    for (int k = 0; k < 5; k++)
+    {
+        cout << values[k] << "  ";
+        stack.push(values[k]);
+    }
+    cout << "\nPopping...\n";
+    while (!stack.isEmpty())
+    {
+        stack.pop(value);
+        cout << value << "  ";
+    }
+    cout << endl;
+
     return 0;
 }
