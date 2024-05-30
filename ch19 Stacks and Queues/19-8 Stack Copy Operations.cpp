22d2bba9e10960d3a75d7130cb7c3cd77ccd25e2
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-30 11:18:32 -0500

    Update 19-8 Stack Copy Operations.cpp

--- ch19 Stacks and Queues/19-8 Stack Copy Operations.cpp
+++ ch19 Stacks and Queues/19-8 Stack Copy Operations.cpp
@@ -96,24 +96,5 @@ int main()
     }
     cout << "\n";
 
-    // Pop three values
-    for (int k = 1; k <= 3; k++)
-    {
-       cout << "Pop: ";
-       stack.pop(popped_value);
-       cout << popped_value << endl;
-    }
-
-    // Stack is now empty, a pop will cause an exception
-    try
-    {
-       cout << "\nAttempting to pop again... ";
-       stack.pop(popped_value);
-    }
-    catch (DynIntStack::Underflow)
-    {
-       cout << "Underflow exception occured.\n";
-    }
-
     return 0;
 }
