e3da3b02646cd100ead78e318fc36fd96e057174
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-30 12:16:29 -0500

    Create 19-9 File Reverser.cpp

---
+++ ch19 Stacks and Queues/19-9 File Reverser.cpp
@@ -0,0 +1,13 @@
+/*Write a program that opens a text file and reads its contents into a stack of characters.
+The program should then pop the characters from the stack and save them in a second text file.
+The order of the characters saved in the second file should be the reverse of their order in the first file.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
