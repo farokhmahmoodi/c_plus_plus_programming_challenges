60bf3daa479619d3a6eba9373bc0cb940519ac28
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-05-31 13:34:14 -0500

    Create 19-12 Stack-based Binary Search.cpp

---
+++ ch19 Stacks and Queues/19-12 Stack-based Binary Search.cpp
@@ -0,0 +1,10 @@
+/*Imitate the technique of Section 19.7 and use a stack to remove recursion from the binary search algorithm.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
