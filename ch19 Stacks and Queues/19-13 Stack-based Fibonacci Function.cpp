d13acee6e37bf3fb32d0e4632cb4114470134950
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-06-02 12:21:06 -0500

    Add files via upload

---
+++ ch19 Stacks and Queues/19-13 Stack-based Fibonacci Function.cpp
@@ -0,0 +1,31 @@
+﻿/*Use a stack to remove recursion from the implementation of the Fibonacci function discussed in Section 14.4.*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int fib(int);   // Function prototype
+
+int main()
+{
+    cout << "The first 10 Fibonacci numbers are:\n";
+    for (int x = 0; x < 10; x++)
+        cout << fib(x) << " ";
+    cout << endl;
+    return 0;
+}
+
+//*****************************************
+// Function fib. Accepts an int argument  *
+// in n. This function returns the nth    *
+// Fibonacci number.                      *
+//*****************************************
+int fib(int n)
+{
+    if (n <= 0)        //base case
+        return 0;
+    else if (n == 1)   //base case
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
