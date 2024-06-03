018ebc547d9fe3ac07e5dddffdce372e7cd7d1bc
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-06-03 10:30:38 -0500

    Create 19-14 Stack-based Evaluation of Postfix Expressions.cpp

---
+++ ch19 Stacks and Queues/19-14 Stack-based Evaluation of Postfix Expressions.cpp
@@ -0,0 +1,18 @@
+/*Write a program that reads postfix expressions and prints their values.
+Each input expression should be entered on its own line, and the program should terminate when the user enters a blank line.
+Assume that there are only binary operators and that the expressions contain no variables. Your program should use a stack.
+Here are sample input–output pairs:
+        78                  78
+        78 6 +              84
+        78 6 + 9 2 - /      12
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
