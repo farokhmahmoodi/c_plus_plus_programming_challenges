9771c1adfbc6a5e7ddf7487760847233b4e9d0c4
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-06-11 09:20:06 -0500

    Create 20-1 Simple Binary Search Tree Class.cpp

---
+++ ch20 Binary Trees/20-1 Simple Binary Search Tree Class.cpp
@@ -0,0 +1,24 @@
+/*Write a class for implementing a simple binary search tree capable of storing numbers. The class should have member functions
+
+void insert(double x)
+bool search(double x)
+void inorder(vector <double> & v )
+
+The insert function should not use recursion directly or indirectly by calling a recursive function.
+The search function should work by calling a private recursive member function
+
+bool search(double x, BtreeNode *t)
+
+The inorder function is passed an initially empty vector v: it fills v with the inorder list of numbers stored in the binary search tree.
+Demonstrate the operation of the class using a suitable driver program.*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+
+    return 0;
+}
